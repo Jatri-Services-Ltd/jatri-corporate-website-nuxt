4bb274f39395b63938b44e2bf68e3e56d8cfc4d7
mohaimenJatri <mohaimen.jatri@gmail.com>
2023-02-16 17:09:52 +0600

    career demo data store added

---
+++ dataStore/career.js
@@ -0,0 +1,52 @@
+const careerList = [
+    {
+        "title": "UX Designer",
+        "slug": "ux-designer",
+        "iconPath": "/assets/images/job_circular/creative_head.jpg",
+        "deadline": "22th January, 2023",
+        "description": "<p class=\"font-thin body_text mt-4\"><u>Job Description:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Understand product specifications, and business needs and create the design to deliver a user-friendly interface</li>\n" +
+            "<li>Translate concepts into user flows, wireframes, mockups, and prototypes that lead to intuitive user experiences.</li>\n" +
+            "<li>Design according to the flows, detailed wireframes, high-fidelity mockups, to clickable prototypes to express the vision.</li>\n" +
+            "<li>Research and conduct informal testing to validate design directions and try to generate key features.</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Additional Requirements:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>At least 2 years of experience and a strong portfolio of related design projects</li>\n" +
+            "<li>Understanding of user-centered design (UCD), planning and conducting user research, user testing, A/B testing, rapid prototyping, heuristic analysis, usability, and accessibility concerns</li>\n" +
+            "<li>Proficient in Figma (is a must), Adobe Xd, Invision, Framer, Zeplin, Marvel, etc.</li>\n" +
+            "<li>Knowledge of Adobe Illustrator and Photoshop is a plus</li>\n" +
+            "<li>Knowledge about design principles, patterns, and familiarity with leading design systems like Human Interface Guidelines, Material Design, etc.</li>\n" +
+            "<li>Ability to prioritize and manage several milestones and projects efficiently</li>\n" +
+            "<li>Collaborate with the product, engineering, and management teams to ensure the creation and delivery of tailored experiences for the digital user.</li>\n" +
+            "<li>Incorporate customer feedback, usage metrics, and usability findings into the design to enhance user experience.</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Education &amp; Experience:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Graduate or university students</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Benefits:</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Lunch</li>\n" +
+            "<li>Festival bonus</li>\n" +
+            "<li>Yearly salary review</li>\n" +
+            "</ul>\n" +
+            "<p class=\"font-thin body_text mt-4\"><u>Number of vacancy</u></p>\n" +
+            "<ul class=\"list-disc ml-4\">\n" +
+            "<li>01</li>\n" +
+            "</ul>",
+        "short_description": "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Understand product specifications, and business needs and create the design to deliver a user-friendly interface</li>\n" +
+            "<li>Translate concepts into user flows, wireframes, mockups, and prototypes that lead to intuitive user experiences.</li>\n" +
+            "<li>Design according to the flows, detailed wireframes, high-fidelity mockups, to clickable prototypes to express the vision.</li>\n" +
+            "<li>Research and conduct informal testing to validate design directions and try to generate key features.</li>\n" +
+            "</ul>",
+        "employment_type": "Full Time",
+        "salary": "20000-30000",
+        "location": "Dhaka",
+        "mail_to": "hr@jatri.co",
+    }
+]
+
+export default careerList;
