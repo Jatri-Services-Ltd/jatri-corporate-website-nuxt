311904ae3db885a008743a4cb5198d7c08a57fcf
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-02-19 12:39:43 +0600

    Software Engineer(Backend) added in json

--- dataStore/career.js
+++ dataStore/career.js
@@ -1,5 +1,53 @@
 const careerList = [
     {
+        "title": "Software Engineer (Backend) ",
+        "slug": "software-engineer-laravel",
+        "iconPath": "/assets/images/job_circular/team_lead.jpg",
+        "deadline": "25<sup>th</sup> January, 2023",
+        "description": `<ul class="list-disc ml-4">
+        <li>Understand product specifications, and business needs and create the design to deliver a user-friendly interface</li>
+        <li>Translate concepts into user flows, wireframes, mockups, and prototypes that lead to intuitive user experiences.</li>
+        <li>Design according to the flows, detailed wireframes, high-fidelity mockups, to clickable prototypes to express the vision.</li>
+        <li>Research and conduct informal testing to validate design directions and try to generate key features.</li>
+        </ul>
+        <p class="font-thin body_text mt-4"><u>Additional Requirements:</u></p>
+        <ul class="list-disc ml-4">
+        <li>At least 2 years of experience and a strong portfolio of related design projects</li>
+        <li>Understanding of user-centered design (UCD), planning and conducting user research, user testing, A/B testing, rapid prototyping, heuristic analysis, usability, and accessibility concerns</li>
+        <li>Proficient in Figma (is a must), Adobe Xd, Invision, Framer, Zeplin, Marvel, etc.</li>
+        <li>Knowledge of Adobe Illustrator and Photoshop is a plus</li>
+        <li>Knowledge about design principles, patterns, and familiarity with leading design systems like Human Interface Guidelines, Material Design, etc.</li>
+        <li>Ability to prioritize and manage several milestones and projects efficiently</li>
+        <li>Collaborate with the product, engineering, and management teams to ensure the creation and delivery of tailored experiences for the digital user.</li>
+        <li>Incorporate customer feedback, usage metrics, and usability findings into the design to enhance user experience.</li>
+        </ul>
+        <p class="font-thin body_text mt-4"><u>Education &amp; Experience:</u></p>
+        <ul class="list-disc ml-4">
+        <li>Graduate or university students</li>
+        </ul>
+        <p class="font-thin body_text mt-4"><u>Benefits:</u></p>
+        <ul class="list-disc ml-4">
+        <li>Lunch</li>
+        <li>Festival bonus</li>
+        <li>Yearly salary review</li>
+        </ul>
+        <p class="font-thin body_text mt-4"><u>Number of vacancy</u></p>
+        <ul class="list-disc ml-4">
+        <li>01</li>
+        </ul>`,
+        "short_description": "<ul class=\"list-disc ml-4\">\n" +
+            "<li>Understand product specifications, and business needs and create the design to deliver a user-friendly interface</li>\n" +
+            "<li>Translate concepts into user flows, wireframes, mockups, and prototypes that lead to intuitive user experiences.</li>\n" +
+            "<li>Design according to the flows, detailed wireframes, high-fidelity mockups, to clickable prototypes to express the vision.</li>\n" +
+            "<li>Research and conduct informal testing to validate design directions and try to generate key features.</li>\n" +
+            "</ul>",
+        "employment_type": "Full Time",
+        "salary": "20000-30000",
+        "location": "Dhaka",
+        "mail_to": "hr@jatri.co",
+    },
+
+    {
         "title": "UX Designer",
         "slug": "ux-designer",
         "iconPath": "/assets/images/job_circular/creative_head.jpg",
