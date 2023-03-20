ecda7417b47cdedfb2334d4d9165e505881856c1
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-03-20 17:31:18 +0600

    mobile nav button color changed

--- components/app/WhiteNavbar.vue
+++ components/app/WhiteNavbar.vue
@@ -37,7 +37,7 @@ const switchLocalePath = useSwitchLocalePath()
 <!--      {{i18n.locales}}-->
 
       <label
-          class="block lg:hidden cursor-pointer flex items-center px-3 py-2 border rounded border-teal-400 hover:corporate_color hover:border-gray-400"
+          class="block lg:hidden cursor-pointer flex items-center px-3 py-2 border rounded border-corporate-400 hover:corporate_color hover:border-gray-400"
           for="menu-toggle">
         <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
