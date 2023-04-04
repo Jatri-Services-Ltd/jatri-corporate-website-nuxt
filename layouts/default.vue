699e1963fe68fb63fd90ace4db991b1a5a7efc5d
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-04-04 15:46:07 +0600

    header and footer new component create

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,16 @@
 <template>
     <div :class="$i18n.locale == 'bn' ? 'font-banglaFont' : 'font-sans'">
-      <WhiteNavbar />
+      <PrimaryNavbar />
       <slot />
 
-      <Footer />
+      <PrimaryFooter />
     </div>
 </template>
 <script setup>
 import WhiteNavbar from "~/components/app/WhiteNavbar";
+import PrimaryNavbar from "~/components/app/PrimaryNavbar";
 import Footer from "~/components/app/Footer";
+import PrimaryFooter from "~/components/app/PrimaryFooter";
 </script>
 
 <style>
