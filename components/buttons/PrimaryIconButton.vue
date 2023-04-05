989a88049352d118dac92dbcbaf88fd0d9093d3e
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-04-05 16:02:21 +0600

    hero section design

---
+++ components/buttons/PrimaryIconButton.vue
@@ -0,0 +1,6 @@
+<template>
+    <button class="flex justify-center items-center gap-2.5 rounded-full bg-primary px-8 py-3 xl:py-4">
+        <img src="~/assets/images/svg/download-white.svg" alt="Download icon">
+        <span class="text-base xl:text-xl font-medium text-white">Download app</span>
+    </button>
+</template>
--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -1,16 +1,26 @@
+<script setup>
+import PrimaryIconButton from "~/components/buttons/PrimaryIconButton";
+</script>
+
 <style scoped>
 * {
     font-family: 'Inter', sans-serif;
 }
+p {
+    @apply text-body
+}
 </style>
 
 <template>
-    <div>
-        <!-- Hero Section -->
-        <div class="bg-[#FEF2F0] h-[800px]">
-            <div class="pt-[120px] text-center">
-                <h1 class="w-full xl:w-[780px] block m-auto text-[64px] leading-[72px] font-bold">Empowering Bangladesh to travel better!</h1>
-                <p class="w-full xl:w-[572px] block m-auto pt-6 pb-8 text-secondaryBlack text-xl">Lorem ipsum dolor sit amet consectetur. Suspendisse cras arcu est et dignissim non lacus neque sit.</p>
+    <!-- Hero Section -->
+    <div class="h-[431px] xl:h-[800px] relative w-full bg-[url('~/assets/images/svg/hero-bg.svg')] bg-no-repeat bg-cover bg-bottom">
+        <!-- <img src="~/assets/images/svg/hero-bg.svg" alt="Hero bg img" class="w-full absolute -z-10"> -->
+
+        <div class="pt-12 xl:pt-[120px] px-4 text-center">
+            <h1 class="w-full xl:w-[780px] block m-auto text-[32px] leading-10 xl:text-[64px] xl:leading-[72px] font-bold">Empowering Bangladesh to travel better!</h1>
+            <p class="w-full xl:w-[572px] block m-auto pt-6 pb-8 text-secondaryBlack text-sm xl:text-xl">Lorem ipsum dolor sit amet consectetur. Suspendisse cras arcu est et dignissim non lacus neque sit.</p>
+            <div class="inline-block m-auto">
+                <PrimaryIconButton />
             </div>
         </div>
     </div>
