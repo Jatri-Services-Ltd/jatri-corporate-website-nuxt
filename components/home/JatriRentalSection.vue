cfbc43f2c19ad5b11b9ab61e98b0a59164033ceb
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-04-06 13:14:19 +0600

    jatri rental section update

--- components/home/JatriRentalSection.vue
+++ components/home/JatriRentalSection.vue
@@ -47,47 +47,47 @@
                     <div class="bg-[#FCD7D3] py-2.5 overflow-hidden transform -rotate-[5deg] -translate-x-6 w-[2000px] absolute -top-20 z-0">
                         <div class="flex w-max">
                             <div class="rental-animation2">
-                                <img src="../img/svg/percentage-round-colorful.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/light-wheel.svg" alt="percentage icon">
                                 <p class="text-[#F7A299] text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation2">
-                                <img src="../img/svg/percentage-round-colorful.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/light-wheel.svg" alt="percentage icon">
                                 <p class="text-[#F7A299] text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation2">
-                                <img src="../img/svg/percentage-round-colorful.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/light-wheel.svg" alt="percentage icon">
                                 <p class="text-[#F7A299] text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation2">
-                                <img src="../img/svg/percentage-round-colorful.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/light-wheel.svg" alt="percentage icon">
                                 <p class="text-[#F7A299] text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation2">
-                                <img src="../img/svg/percentage-round-colorful.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/light-wheel.svg" alt="percentage icon">
                                 <p class="text-[#F7A299] text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation2">
-                                <img src="../img/svg/percentage-round-colorful.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/light-wheel.svg" alt="percentage icon">
                                 <p class="text-[#F7A299] text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation2">
-                                <img src="../img/svg/percentage-round-colorful.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/light-wheel.svg" alt="percentage icon">
                                 <p class="text-[#F7A299] text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation2">
-                                <img src="../img/svg/percentage-round-colorful.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/light-wheel.svg" alt="percentage icon">
                                 <p class="text-[#F7A299] text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation2">
-                                <img src="../img/svg/percentage-round-colorful.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/light-wheel.svg" alt="percentage icon">
                                 <p class="text-[#F7A299] text-xl font-medium">Fixed & low fare rides</p>
                             </div>
                         </div>
@@ -96,54 +96,54 @@
                     <div class="bg-[#F04935] py-2.5 overflow-hidden transform rotate-[5deg] -translate-x-6 w-[2000px] z-10">
                         <div class="flex w-max">
                             <div class="rental-animation">
-                                <img src="../img/svg/percentage-round.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/percentage-round.svg" alt="percentage icon">
                                 <p class="text-white text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation">
-                                <img src="../img/svg/percentage-round.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/percentage-round.svg" alt="percentage icon">
                                 <p class="text-white text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation">
-                                <img src="../img/svg/percentage-round.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/percentage-round.svg" alt="percentage icon">
                                 <p class="text-white text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation">
-                                <img src="../img/svg/percentage-round.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/percentage-round.svg" alt="percentage icon">
                                 <p class="text-white text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation">
-                                <img src="../img/svg/percentage-round.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/percentage-round.svg" alt="percentage icon">
                                 <p class="text-white text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation">
-                                <img src="../img/svg/percentage-round.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/percentage-round.svg" alt="percentage icon">
                                 <p class="text-white text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation">
-                                <img src="../img/svg/percentage-round.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/percentage-round.svg" alt="percentage icon">
                                 <p class="text-white text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation">
-                                <img src="../img/svg/percentage-round.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/percentage-round.svg" alt="percentage icon">
                                 <p class="text-white text-xl font-medium">Fixed & low fare rides</p>
                             </div>
 
                             <div class="rental-animation">
-                                <img src="../img/svg/percentage-round.svg" alt="percentage icon">
+                                <img src="~/assets/images/svg/percentage-round.svg" alt="percentage icon">
                                 <p class="text-white text-xl font-medium">Fixed & low fare rides</p>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <img src="../img/svg/rental-sec-bg.svg" alt="rental bg img" class="w-full rounded-3xl">
+                <img src="~/assets/images/svg/rental-sec-bg.svg" alt="rental bg img" class="w-full rounded-3xl">
             </div>
         </div>
    </div>
