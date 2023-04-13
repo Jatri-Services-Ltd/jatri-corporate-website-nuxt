6dc8c5795cddab0c216d1f5bf155939179f5251f
mohaimenJatri <mohaimen.jatri@gmail.com>
2023-04-13 14:12:19 +0600

    add new component

--- components/app/PrimaryFooter.vue
+++ components/app/PrimaryFooter.vue
@@ -1,3 +1,114 @@
 <template>
-    Primary Footer
+  <footer class="mt-10">
+    <div class="hidden md:block">
+      <img src="~/assets/images/svg/footer-img.svg" alt="Footer img" width="100%">
+    </div>
+
+    <div class="custom-container bg-primary pt-6 lg:pt-12 pb-6 lg:pb-8">
+      <div class="grid lg:grid-cols-4 lg:justify-between">
+        <div class="lg:col-span-1 row-span-1 border-b border-[#F6958A] lg:border-0 pb-6 lg:pb-0 mb-6 lg:mb-0">
+          <div class="flex justify-between items-center lg:block">
+            <img src="~/assets/images/svg/footer-white-logo.svg" alt="Jatri Logo">
+            <div class="flex items-center gap-10 xl:gap-14 lg:mt-8">
+              <img src="~/assets/images/svg/facebook-white.svg" alt="Facebook icon">
+              <img src="~/assets/images/svg/youtube-white.svg" alt="Youtube icon">
+              <img src="~/assets/images/svg/linkedin-white.svg" alt="Linkedin icon">
+            </div>
+          </div>
+
+          <div class="mt-8">
+            <p class="text-base text-white font-medium mb-4">Download our app</p>
+            <img src="~/assets/images/svg/google-play-badge.svg" alt="Google play badge">
+          </div>
+        </div>
+
+        <div class="lg:col-span-3 row-span-2">
+          <div class="grid grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-0 pb-6">
+            <div class="lg:px-12 lg:border-x border-x-[#F6958A]">
+              <h4 class="text-white text-xl lg:text-2xl font-medium mb-4 lg:mb-6">Products</h4>
+              <ul class="grid gap-4">
+                <li>
+                  <a class="text-white font-normal text-base" href="#">Jatri Business</a>
+                </li>
+                <li>
+                  <a class="text-white font-normal text-base" href="#">Jatri Rentals</a>
+                </li>
+                <li>
+                  <a class="text-white font-normal text-base" href="#">Jatri Ticket</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="lg:px-12 row-span-2 lg:row-span-1">
+              <h4 class="text-white text-xl lg:text-2xl font-medium mb-4 lg:mb-6">About us</h4>
+              <ul class="grid gap-4">
+                <li>
+                  <a class="text-white font-normal text-base" href="#">About us</a>
+                </li>
+                <li>
+                  <a class="text-white font-normal text-base" href="#">Career</a>
+                </li>
+                <li>
+                  <a class="text-white font-normal text-base" href="#">Blogs</a>
+                </li>
+                <li>
+                  <a class="text-white font-normal text-base" href="#">Contact us</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="mt-2 lg:mt-0 lg:px-12 lg:border-x border-x-[#F6958A]">
+              <h4 class="text-white text-xl lg:text-2xl font-medium mb-4 lg:mb-6">Others</h4>
+              <ul class="grid gap-4">
+                <li>
+                  <a class="text-white font-normal text-base" href="#">Ride sharing guideline</a>
+                </li>
+                <li>
+                  <a class="text-white font-normal text-base" href="#">Enlistment certificate</a>
+                </li>
+                <li>
+                  <a class="text-white font-normal text-base" href="#">COVID-19 info</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="py-0 lg:py-6 border-t border-[#F6958A] grid grid-cols-1 lg:grid-cols-3 divide-y divide-[#F6958A] lg:divide-y-0 lg:divide-x">
+            <div class="py-4 flex items-center gap-3.5 pl-0 lg:pl-5 xl:pl-12">
+              <img src="~/assets/images/svg/question-white-icon.svg" alt="Question icon">
+              <a class="text-white font-medium text-base" href="mailto:support@jatri.co">support@jatri.co</a>
+            </div>
+            <div class="py-4 flex lg:justify-center items-center gap-3.5">
+              <img src="~/assets/images/svg/phone-white-icon.svg" alt="Question icon">
+              <a class="text-white font-medium text-base" href="tel:09642080808">09642080808</a>
+            </div>
+            <div class="py-4 flex items-center gap-3.5 pl-0 lg:pl-5 pr-0 lg:pr-5 xl:pr-12">
+              <img src="~/assets/images/svg/map-white-icon.svg" alt="Map icon">
+              <a class="text-white font-medium text-base" href="">Road 24, House 15, Gulshan-02, Dhaka 1212</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="border-t border-[#F6958A] grid lg:grid-cols-2">
+        <div class="pt-6 order-2 lg:order-1 flex justify-center lg:justify-start items-center gap-2.5">
+          <img src="~/assets/images/svg/copyright-white-icon.svg" alt="">
+          <p class="text-white text-base font-medium">All rights reserved @Jatri 2023</p>
+        </div>
+
+        <div class="order-1 lg:order-2 place-self-center lg:place-self-end border-b border-b-[#F6958A] lg:border-b-0 py-6 lg:pb-0 grid grid-cols-3 lg:flex items-center lg:gap-8 text-center lg:text-right">
+          <a class="text-white font-medium text-sm underline" href="#">Terms and condition</a>
+          <a class="text-white font-medium text-sm underline border-x border-[#F6958A] lg:border-x-0 px-4 sm:px-8 lg:px-0" href="#">Return & refund policy</a>
+          <a class="text-white font-medium text-sm underline" href="#">Privacy policy</a>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
+
+<style scoped>
+* {
+  font-family: 'Inter', sans-serif;
+}
+</style>
--- components/app/PrimaryNavbar.vue
+++ components/app/PrimaryNavbar.vue
@@ -1,10 +1,74 @@
-<script>
+<template>
+  <div class="flex items-center justify-between px-2 py-4 lg:px-28 lg:py-4">
+    <img class="h-8 w-16 md:hidden" src="@/assets/images/svg/jatri-logo.svg" alt="">
+    <div class="hidden md:flex gap-x-12">
+      <img class="h-20 w-28" src="@/assets/images/svg/jatri-logo.svg" alt="">
+      <div class="hidden md:flex gap-x-10 items-center w-full">
+        <nuxt-link to="/" class="text-sm font-medium text-black border-b-4 border-black inline-block text-center">Home</nuxt-link>
+        <nuxt-link to="/" class="text-sm font-medium text-black text-center inline-block">About us</nuxt-link>
+        <nuxt-link to="/" class="text-sm font-medium text-black text-center inline-block">Career</nuxt-link>
+        <nuxt-link to="/" class="text-sm font-medium text-black text-center inline-block">Blogs</nuxt-link>
+        <nuxt-link to="/" class="text-sm font-medium text-black text-center inline-block">Contact us</nuxt-link>
+      </div>
+    </div>
 
+    <div class="hidden md:flex gap-x-6 items-center">
+      <select class="focus:outline-none" name="language" id="">
+        <option value="English">English</option>
+        <option value="Bangla">Bangla</option>
+      </select>
+
+      <a href="https://rental.jatri.co/" target="_blank" class="flex gap-x-2 items-center text-lg leading-6 font-medium rounded-full border border-[#EDEDED] px-4 py-3"><img src="@/assets/images/svg/car-svg.svg" alt=""> Rental</a>
+      <a href="https://ticket.jatri.co/" target="_blank" class="bg-corporate text-white flex gap-x-2 items-center text-lg leading-6 font-medium rounded-full border border-[#EDEDED] pl-4 pr-6 py-3"><img src="@/assets/images/svg/ticket.svg" alt=""> Ticket</a>
+    </div>
+
+    <div class="flex gap-x-2 md:hidden">
+      <a href="https://rental.jatri.co/" target="_blank" class="flex gap-x-2 justify-center items-center text-xs leading-6 font-medium rounded-full border border-[#EDEDED] px-4 py-3 w-[100px] h-9"><img src="@/assets/images/svg/car-svg.svg" alt=""> Rental</a>
+      <a href="https://ticket.jatri.co/" target="_blank" class="bg-corporate text-white flex justify-center gap-x-2 items-center text-xs leading-6 font-medium rounded-full border border-[#EDEDED] w-[100px] h-9"><img src="@/assets/images/svg/ticket.svg" alt=""> Ticket</a>
+      <button @click="sidebarOpen = !sidebarOpen"><img src="@/assets/images/svg/3bar-icon.svg" alt=""></button>
+    </div>
+
+
+    <!--      mobile menu item-->
+    <div v-if="sidebarOpen" class="bg-white absolute inset-y-0 right-0 -translate-x-[300px]" :class="sidebarOpen ? 'absolute slide-in  z-50 translate-x-0 transition delay-300 duration-300' : ''">
+      <div class="h-screen w-[300px] gap-y-10">
+        <div class="flex justify-between items-center border-b border-[#EDEDED] py-4">
+          <p class="font-medium text-sm text-black pl-2">Menu</p>
+          <button @click="sidebarOpen = false" class="pr-2"><img src="@/assets/images/svg/cross-icon.svg" alt=""></button>
+        </div>
+        <div class="flex flex-col gap-y-2 px-2">
+          <nuxt-link to="/" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] py-4">About us</nuxt-link>
+          <nuxt-link to="/" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] py-4">Career</nuxt-link>
+          <nuxt-link to="/" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] py-4">Blogs</nuxt-link>
+          <nuxt-link to="/" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] py-4">Contact us</nuxt-link>
+          <select class="focus:outline-none py-4" name="language" id="">
+            <option value="English">English</option>
+            <option value="Bangla">Bangla</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <div v-if="sidebarOpen" @click="sidebarOpen = false" class="fixed inset-0 z-30 bg-black bg-opacity-30 h-full w-full cursor-default transition duration-300 ease-in-out"></div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const sidebarOpen = ref(false)
 </script>
 
-<template>
-    <div class="flex justify-between items-center p-5">
-        <p class="text-lg font-semibold">Primary Navbar</p>
-        <router-link to="/new-home" class="bg-primary rounded-full px-5 py-2 text-white text-sm">New Homepage</router-link>
-    </div>
-</template>
+<style>
+.slide-in{
+  position: absolute;
+  right: -50%;
+  animation: slide-in 0.5s forwards;
+}
+
+@keyframes slide-in {
+  100% {
+    right: 0;
+  }
+
+}
+
+</style>
