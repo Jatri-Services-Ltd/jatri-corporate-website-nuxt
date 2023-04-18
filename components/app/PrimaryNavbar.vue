5e8baf649eeca8bc6e611096fbf4a885e057ab7b
Saiem <shahedsaiem.jatri@gmail.com>
2023-04-18 12:33:18 +0600

    sidebar open scroll close

--- components/app/PrimaryNavbar.vue
+++ components/app/PrimaryNavbar.vue
@@ -24,30 +24,30 @@
         <div class="flex gap-x-4 lg:hidden py-4">
           <a href="https://rental.jatri.co/" target="_blank" class="flex gap-x-2 justify-center items-center text-xs leading-6 font-medium rounded-full border border-[#EDEDED] px-4 py-3 w-[100px] h-9"><img src="@/assets/images/svg/car-svg.svg" alt=""> Rental</a>
           <a href="https://ticket.jatri.co/" target="_blank" class="bg-corporate text-white flex justify-center gap-x-2 items-center text-xs leading-6 font-medium rounded-full border border-[#EDEDED] w-[100px] h-9"><img src="@/assets/images/svg/ticket.svg" alt=""> Ticket</a>
-          <button @click="sidebarOpen = !sidebarOpen"><img src="@/assets/images/svg/3bar-icon.svg" alt=""></button>
+          <button @click="toggleModal"><img src="@/assets/images/svg/3bar-icon.svg" alt=""></button>
         </div>
       
       
       <!--      mobile menu item-->
         <div v-if="sidebarOpen" class="bg-white absolute inset-y-0 right-0 -translate-x-[300px] slide" :class="sidebarOpen ? 'absolute  z-50 translate-x-0 transition delay-300 duration-300' : ''">
           <div class="h-screen w-[300px] gap-y-10">
-            <div class="flex justify-between items-center border-b border-[#EDEDED] py-4">
-              <p class="font-medium text-sm text-black pl-2">Menu</p>
-              <button @click="sidebarOpen = false" class="pr-2"><img src="@/assets/images/svg/cross-icon.svg" alt=""></button>
+            <div class="flex justify-between items-center border-b border-[#EDEDED] p-5">
+              <p class="font-medium text-sm text-black">Menu</p>
+              <button @click="closeModal" class=""><img src="@/assets/images/svg/cross-icon.svg" alt=""></button>
             </div>
             <div class="flex flex-col gap-y-2 px-2">
-              <nuxt-link :to="localePath('/about-us')" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] py-4">About us</nuxt-link>
-              <nuxt-link :to="localePath('/career')" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] py-4">Career</nuxt-link>
-              <nuxt-link :to="localePath('/blog')" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] py-4">Blogs</nuxt-link>
-              <nuxt-link :to="localePath('/contact-us')" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] py-4">Contact us</nuxt-link>
-              <select class="focus:outline-none py-4" name="language" id="">
+              <nuxt-link :to="localePath('/about-us')" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] p-4">About us</nuxt-link>
+              <nuxt-link :to="localePath('/career')" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] p-4">Career</nuxt-link>
+              <nuxt-link :to="localePath('/blog')" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] p-4">Blogs</nuxt-link>
+              <nuxt-link :to="localePath('/contact-us')" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] p-4">Contact us</nuxt-link>
+              <select  class="focus:outline-none p-4" name="language" id="">
                 <option value="English">English</option>
                 <option value="Bangla">Bangla</option>
               </select>
             </div>
           </div>
         </div>
-        <div v-if="sidebarOpen" @click="sidebarOpen = false" class="fixed inset-0 z-30 bg-black bg-opacity-30 h-full w-full cursor-default transition duration-300 ease-in-out"></div>
+        <div v-if="sidebarOpen" @click="closeModal" class="fixed inset-0 z-30 bg-black bg-opacity-30 h-full w-full cursor-default transition duration-300 ease-in-out"></div>
       </div>
     </nav>
 
@@ -56,16 +56,19 @@
 <script setup>
 import {ref} from "vue";
 const sidebarOpen = ref(false)
-// const getBody = document.getElementsByTagName('body')[0];
-// const toggleModal = () => {
-//   sidebarOpen.value = !sidebarOpen.value;
-//   if(sidebarOpen.value) {
-//     getBody.style.overflow = 'hidden';
-//   }
-//   else {
-//     getBody.style.overflow = 'auto';
-//   }
-// }
+
+const closeModal = ()=>{
+  let getBody = document.getElementsByTagName('body')[0];
+  sidebarOpen.value = false
+  getBody.style.overflow = 'auto';
+}
+const toggleModal = () => {
+  sidebarOpen.value = true;
+  let getBody = document.getElementsByTagName('body')[0];
+  if(sidebarOpen.value) {
+    getBody.style.overflow = 'hidden';
+  }
+}
 </script>
 
 <style>
@@ -89,5 +92,8 @@ const sidebarOpen = ref(false)
     right: 0;
   }
 }
+
+
+
 
 </style>
