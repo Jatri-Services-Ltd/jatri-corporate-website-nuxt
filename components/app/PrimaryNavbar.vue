8cf9e20bcc36e34b449fd1dbd67129c464922ecb
farjanaIslam09 <farjana.jatri@gmail.com>
2023-04-18 12:25:44 +0600

    update Select language selector design

--- components/app/PrimaryNavbar.vue
+++ components/app/PrimaryNavbar.vue
@@ -14,10 +14,26 @@
             </div>
           </div>
         <div class="hidden lg:flex gap-x-2 lg:gap-x-6 items-center">
-          <select class="focus:outline-none text-[12px]" name="language" id="">
-            <option value="English">English</option>
-            <option value="Bangla">Bangla</option>
-          </select>
+          <div class="relative inline-block text-left mr-3">
+                  <div>
+                    <button @click="isOpen = !isOpen" type="button"
+                      class="flex items-center gap-2 text-sm font-medium focus:outline-none"
+                      id="menu-button" aria-expanded="true" aria-haspopup="true">
+                      <span>{{ selectedLanguage }}</span>
+                      <div><img src="~/assets/images/svg/bottom-arrow.svg" alt=""></div>
+                    </button>
+                  </div>
+                  <div v-if="isOpen" class="absolute left-0  z-10 min-w-[120px] origin-top-right rounded-md bg-white focus:outline-none dropdown-box custom-shadow mt-6"
+                      role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                      <div @click="selectLanguage('English')" class="text-gray-500 px-3 transition ease-out duration-200 cursor-pointer" role="none">
+                        <span class="tracking-widest border-b my-2 block py-2 text-sm capitalize">English</span>
+                      </div>
+                      <div @click="selectLanguage('Bangla')" class="text-gray-500 px-3 transition ease-out duration-200 cursor-pointer" role="none">
+                        <span class="tracking-widest my-2 block py-2 text-sm capitalize">Bangla</span>
+                      </div>
+                    </div>
+                </div>
+
           <a href="https://rental.jatri.co/" target="_blank" class="flex gap-x-2 items-center text-[16px] leading-6 font-medium rounded-full border border-[#EDEDED] px-4 py-2 lg:py-3"><img src="@/assets/images/svg/car-svg.svg" alt=""> Rental</a>
           <a href="https://ticket.jatri.co/" target="_blank" class="bg-corporate text-white flex gap-x-2 items-center text-[16px] leading-6 font-medium rounded-full border border-[#EDEDED] pl-4 pr-6 py-2 lg:py-3"><img src="@/assets/images/svg/ticket.svg" alt=""> Ticket</a>
         </div>
@@ -40,10 +56,32 @@
               <nuxt-link :to="localePath('/career')" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] py-4">Career</nuxt-link>
               <nuxt-link :to="localePath('/blog')" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] py-4">Blogs</nuxt-link>
               <nuxt-link :to="localePath('/contact-us')" @click="sidebarOpen = false" class="text-sm font-medium text-black text-left block border-b border-[#EDEDED] py-4">Contact us</nuxt-link>
-              <select class="focus:outline-none py-4" name="language" id="">
+              <!-- <select class="focus:outline-none py-4" name="language" id="">
                 <option value="English">English</option>
                 <option value="Bangla">Bangla</option>
-              </select>
+              </select> -->
+              <div>
+                <div class="relative inline-block text-left">
+                  <div>
+                    <button @click="isOpen = !isOpen" type="button"
+                      class="inline-flex justify-between items-center rounded-md px-5 py-3 text-sm font-medium text-gray-700 shadow-md hover:bg-gray-50 focus:outline-none"
+                      id="menu-button" aria-expanded="true" aria-haspopup="true">
+                      {{ selectedLanguage }}
+                    </button>
+                  </div>
+                  <div v-if="isOpen" class="mt-6">
+                    <div class="absolute left-0 z-10 w-64 origin-top-right rounded-md bg-white focus:outline-none dropdown-box pb-4 custom-shadow"
+                      role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                      <div @click="selectLanguage('English')" class="text-gray-500 hover:text-[#527ff6] px-5 border-l-2 border-l-white hover:border-l-[#527ff6] transition ease-out duration-200 cursor-pointer" role="none">
+                        <span class="tracking-widest border-b my-2 block py-4 text-sm capitalize">English</span>
+                      </div>
+                      <div @click="selectLanguage('Bangla')" class="text-gray-500 hover:text-[#527ff6] px-5 border-l-2 border-l-white hover:border-l-[#527ff6] transition ease-out duration-200 cursor-pointer" role="none">
+                        <span class="tracking-widest border-b my-2 block py-4 text-sm capitalize">Bangla</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -56,6 +94,14 @@
 <script setup>
 import {ref} from "vue";
 const sidebarOpen = ref(false)
+
+const isOpen = ref(false)
+const selectedLanguage = ref('English')
+
+const selectLanguage = (lang) => {
+    selectedLanguage.value = lang
+    isOpen.value = false
+}
 </script>
 
 <style>
@@ -73,6 +119,25 @@ const sidebarOpen = ref(false)
   position: absolute;
   right: -50%;
   animation: slide 0.5s forwards;
+}
+.arrow-up {
+  border-left: 10px solid transparent;
+  border-right: 10px solid transparent;
+  border-bottom: 10px solid white;
+}
+
+.custom-shadow{
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px;
+}
+
+.dropdown-box::before{
+  content: '';
+  border-left: 10px solid transparent;
+  border-right: 10px solid transparent;
+  border-bottom: 10px solid #fff;
+  position: absolute;
+  left: 15px;
+  top: -10px;
 }
 @keyframes slide {
   100% {
