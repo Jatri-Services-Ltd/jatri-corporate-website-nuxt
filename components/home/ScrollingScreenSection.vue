3a660264b12a625b3b2c32995e687ebb0b9bcfd5
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-05-02 12:46:49 +0600

    screen scrolling section localized

--- components/home/ScrollingScreenSection.vue
+++ components/home/ScrollingScreenSection.vue
@@ -13,7 +13,7 @@
         <div class="lg:row-span-2 bg-grey rounded-3xl px-6 md:px-8 grid grid-cols-2 md:grid-cols-1 sm:justify-center items-center gap-4 sm:gap-8 md:gap-0">
           <div class="md:pt-11 md:pb-1">
             <img src="~/assets/images/svg/green-user.svg" alt="screen img" class="w-7 sm:w-auto block md:m-auto">
-            <p class="mt-[18px] text-dark text-base sm:text-2xl font-medium md:text-center">You can bid your fare and choose your desired car.</p>
+            <p class="mt-[18px] text-dark text-base sm:text-2xl font-medium md:text-center">{{ $t('n-screen-scrolling-title1') }}</p>
           </div>
 
           <div class="flex justify-end md:justify-center self-end">
@@ -24,7 +24,7 @@
         <div class="relative lg:row-span-2 bg-grey rounded-3xl px-6 md:px-8 grid grid-cols-2 md:grid-cols-1 sm:justify-center items-center gap-4 sm:gap-8 md:gap-0">
           <div class="md:pt-11 md:pb-1">
             <img src="~/assets/images/svg/security-blue.svg" alt="screen img" class="w-7 sm:w-auto block md:m-auto">
-            <p class="mt-[18px] text-dark text-base sm:text-2xl font-medium md:text-center">Safer trips with our safety tools and features.</p>
+            <p class="mt-[18px] text-dark text-base sm:text-2xl font-medium md:text-center">{{ $t('n-screen-scrolling-title2') }}</p>
           </div>
 
           <div class="flex justify-end md:justify-center self-end">
@@ -33,16 +33,16 @@
         </div>
 
         <div class="md:col-span-2 bg-grey rounded-3xl p-6 lg:p-12 overflow-hidden">
-          <p class="text-base sm:text-xl lg:text-2xl text-[#8D8D8F]">Get ready, Book & GO!</p>
-          <h3 class="custom-heading text-dark text-[28px] leading-9 sm:text-5xl lg:text-4xl xl:text-[40px] font-semibold mt-2 mb-6">One-stop Mobility App</h3>
+          <p class="text-base sm:text-xl lg:text-2xl text-[#8D8D8F]">{{ $t('n-screen-scrolling-subheading') }}</p>
+          <h3 class="custom-heading text-dark text-[28px] leading-9 sm:text-5xl lg:text-4xl xl:text-[40px] font-semibold mt-2 mb-6">{{ $t('n-screen-scrolling-heading3') }}</h3>
           <div class="flex gap-2 sm:gap-6">
             <a href="https://play.google.com/store/apps/details?id=com.jatri.jatriuser&hl=en&gl=US" target="_blank" class="small-device-btn flex justify-center items-center gap-1.5 sm:gap-2.5 rounded-full bg-success border border-success pr-6 sm:pr-8 pl-5 sm:pl-[26px] py-3.5 cursor-pointer">
               <img src="~/assets/images/svg/google-play-icon-white.svg" alt="Google play icon">
-              <span class="text-sm lg:text-base font-medium text-white">Play store</span>
+              <span class="text-sm lg:text-base font-medium text-white">{{ $t('n-play-store') }}</span>
             </a>
             <a class="small-device-btn flex justify-center items-center gap-1.5 sm:gap-2.5 rounded-full bg-transparent border border-[#8D8D8F] pr-6 sm:pr-8 pl-5 sm:pl-[26px] py-3.5 cursor-pointer">
               <img src="~/assets/images/svg/apple-gray-icon.svg" alt="Apple play icon">
-              <span class="text-sm lg:text-base font-medium text-[#8D8D8F]">Coming soon</span>
+              <span class="text-sm lg:text-base font-medium text-[#8D8D8F]">{{ $t('n-coming-soon') }}</span>
             </a>
           </div>
         </div>
