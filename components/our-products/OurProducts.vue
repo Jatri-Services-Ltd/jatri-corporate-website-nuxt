4a17c162b71e7ca7313f9a3740f8d7960c26faa4
tarif <khalidhossain.jatri@gmail.com>
2023-05-22 11:56:49 +0600

    our products design updated

--- components/our-products/OurProducts.vue
+++ components/our-products/OurProducts.vue
@@ -1,6 +1,9 @@
 <template>
-  <section class="bg-[url('~/assets/images/our-products/our-products-banner.png')] bg-no-repeat bg-cover bg-center pt-[220px] sm:pt-56 lg:pt-72 pb-4 lg:pb-16">
-    <div class="custom-container">
+  <section class="relative">
+    <img src="~/assets/images/our-products/our-products-banner.png" alt="Our Products Banner" class="w-full relative hidden sm:block">
+    <img src="~/assets/images/our-products/our-products-banner-mobile.png" alt="Our Products Banner" class="w-full relative sm:hidden">
+
+    <div class="custom-container absolute bottom-4 md:bottom-12 lg:bottom-16 left-0">
       <div class="bg-white w-full md:w-fit px-4 md:pl-10 md:pr-[100px] py-5 md:py-6">
         <h1 class="text-dark text-2xl lg:text-[45px] lg:leading-[60px] font-semibold">
           <span class="text-[#8D8D8F]">Our products</span> <br>
@@ -10,4 +13,3 @@
     </div>
   </section>
 </template>
-
