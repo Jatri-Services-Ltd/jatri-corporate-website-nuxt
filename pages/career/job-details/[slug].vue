91574ee241245e7b19906fcd02f01d932d8b613f
farjanaIslamNipa <farjana.jatri@gmail.com>
2023-07-11 15:42:41 +0600

    Working on job apply modal

--- pages/career/job-details/[slug].vue
+++ pages/career/job-details/[slug].vue
@@ -1,81 +1,135 @@
 <template>
-    <div class="job-details-header text-white py-8">
+    <div id="top" class="job-details-header text-white py-8">
         <div class="custom-container">
             <div class="flex items-center gap-3">
-                <p class="text-sm">Career</p>
+                <p class="text-xs sm:text-sm">Career</p>
                 <div class="relative">
                     <div class="right-arrow absolute top-0 bottom-0 left-0 right-0 m-auto"></div>
                 </div>
-                <p class="text-sm ml-3">Open roles</p>
+                <p class="text-xs sm:text-sm ml-2 md:ml-3">Open roles</p>
                 <div class="relative">
                     <div class="right-arrow absolute top-0 bottom-0 left-0 right-0 m-auto"></div>
                 </div>
-                <p class="text-sm ml-3">{{ jobDetails.title }}</p>
+                <p class="text-xs sm:text-sm ml-2 md:ml-3">{{ jobDetails.title }}</p>
             </div>
-            <h1 class="mt-2 text-4xl leading-[44px] font-medium">Software Engineer (Backend)</h1>
+            <h1 class="mt-2 text-[28px] lg:text-4xl leading-[44px] font-medium">{{ jobDetails.title }}</h1>
         </div>
     </div>
     <div class="custom-container mt-10 mb-[120px]">
-        <div class="grid grid-cols-12 gap-12">
-            <div class="col-span-3">
+        <div class="grid grid-cols-12 gap-8 lg:gap-12">
+            <div class="col-span-12 md:col-span-5 lg:col-span-4">
                 <div class="border border-[#EDEDED] rounded-2xl p-5">
                     <div class="flex items-center gap-5">
                         <div><img class="h-6 w-6" src="/images/career/job-nature-icon.svg" alt="Job nature"></div>
                         <div>
-                            <p class="text-base text-secondaryDark mb-1">Job nature</p>
-                            <p class="text-xl font-medium text-dark">{{ jobDetails.employment_type }}</p>
+                            <p class="text-sm lg:text-base text-secondaryDark mb-1">Job nature</p>
+                            <p class="text-base lg:text-xl font-medium text-dark">{{ jobDetails.employment_type }}</p>
                         </div>
                     </div>
                     <div class="border-b ml-10 pt-4"></div>
                     <div class="flex items-center gap-5 pt-4">
                         <div><img class="h-6 w-6" src="/images/career/salary-range-icon.svg" alt="Salary range"></div>
                         <div>
-                            <p class="text-base text-secondaryDark mb-1">Salary range</p>
-                            <p class="text-xl font-medium text-dark">{{ jobDetails.salary }}</p>
+                            <p class="text-sm lg:text-base text-secondaryDark mb-1">Salary range</p>
+                            <p class="text-base lg:text-xl font-medium text-dark">{{ jobDetails.salary }}</p>
                         </div>
                     </div>
                     <div class="border-b ml-10 pt-4"></div>
                     <div class="flex items-center gap-5 pt-4">
                         <div><img class="h-6 w-6" src="/images/career/job-location-icon.svg" alt="Job location"></div>
                         <div>
-                            <p class="text-base text-secondaryDark mb-1">Job location</p>
-                            <p class="text-xl font-medium text-dark">{{ jobDetails.location }}</p>
+                            <p class="text-sm lg:text-base text-secondaryDark mb-1">Job location</p>
+                            <p class="text-base lg:text-xl font-medium text-dark">{{ jobDetails.location }}</p>
                         </div>
                     </div>                    <div class="border-b ml-10 pt-4"></div>
                     <div class="flex items-center gap-5 pt-4">
                         <div><img class="h-6 w-6" src="/images/career/deadline-icon.svg" alt="Application deadline"></div>
                         <div>
-                            <p class="text-base text-secondaryDark mb-1">Application deadline</p>
-                            <p class="text-xl font-medium text-dark" v-html="jobDetails.deadline"></p>
+                            <p class="text-sm lg:text-base text-secondaryDark mb-1">Application deadline</p>
+                            <p class="text-base lg:text-xl font-medium text-dark" v-html="jobDetails.deadline"></p>
                         </div>
                     </div>
                     <div class="border-b ml-10 pt-4"></div>
                     <div class="flex items-center gap-5 pt-4">
                         <div><img class="h-6 w-6" src="/images/career/vacancy-icon.svg" alt="Vacancy"></div>
                         <div>
-                            <p class="text-base text-secondaryDark mb-1">Vacancy</p>
-                            <p class="text-xl font-medium text-dark">2</p>
+                            <p class="text-sm lg:text-base text-secondaryDark mb-1">Vacancy</p>
+                            <p class="text-base lg:text-xl font-medium text-dark">2</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-span-9">
+            <div class="col-span-12 md:col-span-7 lg:col-span-8">
                 <div v-html="jobDetails.description"></div>
-                <div class="my-12">
-                    <button class="bg-corporate text-white h-[60px] w-[240px] rounded-full flex justify-center items-center font-medium text-xl">Apply Now</button>
+                <div class="mt-6 md:mt-12 mb-8 md:mb-12">
+                    <a href="#top" @click="openModal = !openModal" class="bg-corporate text-white h-[46px] md:h-[60px] w-full md:w-[240px] rounded-full flex justify-center items-center font-medium text-base md:text-xl">Apply Now</a>
                 </div>
                 <div class="border rounded-[10px] px-6 py-5">
-                    <div class="flex gap-4 items-center">
-                        <div class="w-11"><img class="h-11 w-11" src="/images/career/having-trouble-icon.svg" alt="Jatri career"></div>
-                        <div>
-                            <p class="text-dark text-xl font-medium mb-2">Having a trouble with applying?</p>
-                            <p class="text-base pr-0 lg:pr-64">Send you CV or Resume at "<a href="mailto:career@jatri.co">career@jatri.co</a>", do not forget to mention the post name in the subject of the mail.</p>
+                    <div class="flex gap-2 items-center w-full">
+                        <div class="w-[10%]"><img class="h-11 w-full" src="/images/career/having-trouble-icon.svg" alt="Jatri career"></div>
+                        <div class="w-[90%]">
+                            <p class="text-dark text-base md:text-xl font-medium mb-2">Having a trouble with applying?</p>
+                            <p class="text-sm md:text-base pr-0 lg:pr-64">Send you CV or Resume at "<a class="text-[#1E88E5]" href="mailto:career@jatri.co">career@jatri.co</a>", do not forget to mention the post name in the subject of the mail.</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="modal w-[90%] md:w-[700px] rounded-2xl" :class="openModal ? 'modalOpen' : 'modal-hidden'">
+        <div class="px-6 pt-6 border-b">
+            <div class="w-full flex justify-between items-center pb-6">
+                <p class="text-[32px] font-medium">Apply now</p>
+                <button @click="openModal = !openModal"><img src="/images/career/close-icon.svg" alt="Close Modal"></button>
+            </div>
+        </div>
+        <div class="p-6">
+            <p class="text-2xl font-medium mb-6">Personal information</p>
+            <div class="flex gap-8">
+                <div class="w-full">
+                    <label class="text-base font-medium text-dark pb-[10px] block" for="">Full name</label>
+                    <input type="text" class="w-full p-4 bg-[#F7F7F7] placeholder:text-sm placeholder:text-[#676769]" placeholder="Enter full name">
+                </div>
+                <div class="w-full">
+                    <label class="text-base font-medium text-dark pb-[10px] block" for="">Email address</label>
+                    <input type="text" class="w-full p-4 bg-[#F7F7F7] placeholder:text-sm placeholder:text-[#676769]" placeholder="Enter email address">
+                </div>
+            </div>
+            <div class="flex gap-8 mt-6">
+                <div class="w-full">
+                    <label class="text-base font-medium text-dark pb-[10px] block" for="">Mobile number</label>
+                    <input type="text" class="w-full p-4 bg-[#F7F7F7] placeholder:text-sm placeholder:text-[#676769]" placeholder="Enter mobile number">
+                </div>
+                <div class="w-full">
+                    <label class="text-base font-medium text-dark pb-[10px] block" for="">LinkedIn profile</label>
+                    <input type="text" class="w-full p-4 bg-[#F7F7F7] placeholder:text-sm placeholder:text-[#676769]" placeholder="Enter linkedin profile link">
+                </div>
+            </div>
+            <div class="mt-8">
+                <p class="text-2xl font-medium mb-6">Resume / CV</p>
+                <label class="text-base font-medium text-dark pb-[10px] block" for="">Resume / CV</label>
+                <div class="relative overflow-hidden">
+                    <button class="w-full border border-[#1E88E5] border-dashed py-[22px] px-5 text-start rounded-[4px]">
+                        <div v-if="fileName">
+                            {{ fileName }}
+                        </div>
+                        <div class="flex items-center gap-4" :class="fileName ? 'hidden' : ''">
+                            <img src="/images/career/upload-icon.svg" alt="">
+                            <div>
+                                <p class="text-[#1E88E5] text-base font-medium mb-1">Upload resume / cv</p>
+                                <p class="text-xs">File format should be <b>PDF</b> and not more than <b>5MB</b></p>
+                            </div>
+                        </div>
+                    </button>
+                    <input @change="uploadFile" type="file" name="myfile" class="absolute left-0 top-0 opacity-0 w-full h-full cursor-pointer" />
+                </div>
+            </div>
+            <div class="mt-12 mb-4 flex justify-center">
+                <button class="h-[60px] w-[300px] flex justify-center items-center bg-corporate text-white rounded-full text-xl font-medium">Submit</button>
+            </div>
+        </div>
+    </div>
+<div @click="openModal = !openModal" class="overlay" :class="openModal ? '' : 'modal-hidden'"></div>
 </template>
 
 <script setup>
@@ -84,10 +138,17 @@ import careerData from "~/dataStore/career.js";
 const route = useRoute();
 
 const jobDetails = ref({})
+const openModal = ref(false)
+const fileName = ref('')
 
 onMounted(() => {
     jobDetails.value = careerData.find(data => data.slug === route.params.slug)
 })
+
+const uploadFile = (e) => {
+    fileName.value = e.target.files[0].name
+    console.log(fileName.value, 'object');
+}
 </script>
 
 <style scoped>
@@ -107,4 +168,64 @@ onMounted(() => {
 		transparent;
 	transform: rotate(45deg);
 }
+
+/* modal css */
+.show-modal {
+        font-size: 2rem;
+        font-weight: 600;
+        padding: 1.75rem 3.5rem;
+        margin: 5rem 2rem;
+        border: none;
+        background-color: #fff;
+        color: #444;
+        border-radius: 10rem;
+        cursor: pointer;
+    }
+
+    .close-modal {
+        position: absolute;
+        top: 1.2rem;
+        right: 2rem;
+        font-size: 5rem;
+        color: #333;
+        cursor: pointer;
+        border: none;
+        background: none;
+    }
+
+    .modal-hidden {
+        opacity: 0;
+        transition: all 0.3s ease-in-out;
+        -ms-transform: scale(0.7); /* IE 9 */
+        -webkit-transform: scale(0.7); /* Safari */
+        transform: scale(0.1);
+    }
+    .modalOpen{
+        z-index: 60;
+        opacity: 1;
+        transition: all 0.3s ease-in-out;
+        transform: scale(1);
+        transition-delay: 0.1s;
+    }
+
+    .modal {
+        position: absolute;
+        top: 60%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: white;
+        z-index: 10;
+    }
+
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh !important;
+        background-color: rgba(0, 0, 0, 0.6);
+        backdrop-filter: blur(3px);
+        z-index: 5;
+    }
+
 </style>
