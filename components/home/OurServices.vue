ca31fab5123700d9423d34da86ddae87e3131255
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-09-24 11:34:20 +0600

    services image height changed

--- components/home/OurServices.vue
+++ components/home/OurServices.vue
@@ -7,7 +7,7 @@
         <div class="custom-container">
             <div class="grid md:grid-cols-3 gap-4 lg:gap-8">
                 <div class="border border-[#EDEDED] rounded-2xl">
-                    <img class="w-full h-[200px] object-cover rounded-t-2xl" src="~/assets/images/home/intercity.jpg" alt="Bus image">
+                    <img class="w-full h-[160px] sm:h-[200px] 3xl:h-[300px] object-cover rounded-t-2xl" src="~/assets/images/home/intercity.jpg" alt="Bus image">
                     <div class="p-4 lg:px-4 lg:py-6 flex md:flex-col justify-between md:justify-start items-center md:items-start gap-1 md:gap-4">
                         <h3 class="text-2xl font-medium text-dark">{{ $t('n-services-item1-title') }}</h3>
                         <div class="inline-block">
@@ -22,7 +22,7 @@
                 </div>
 
                 <div class="border border-[#EDEDED] rounded-2xl">
-                    <img class="w-full h-[200px] object-cover rounded-t-2xl" src="~/assets/images/home/launch.jpg" alt="Bus image">
+                    <img class="w-full h-[160px] sm:h-[200px] 3xl:h-[300px] object-cover rounded-t-2xl" src="~/assets/images/home/launch.jpg" alt="Bus image">
                     <div class="p-4 lg:px-4 lg:py-6 flex md:flex-col justify-between md:justify-start items-center md:items-start gap-1 md:gap-4">
                         <h3 class="text-2xl font-medium text-dark">{{ $t('n-services-item2-title') }}</h3>
                         <div class="inline-block">
@@ -37,7 +37,7 @@
                 </div>
 
                 <div class="border border-[#EDEDED] rounded-2xl">
-                    <img class="w-full h-[200px] object-cover rounded-t-2xl" src="~/assets/images/home/rental.jpg" alt="Bus image">
+                    <img class="w-full h-[160px] sm:h-[200px] 3xl:h-[300px] object-cover rounded-t-2xl" src="~/assets/images/home/rental.jpg" alt="Bus image">
                     <div class="p-4 lg:px-4 lg:py-6 flex md:flex-col justify-between md:justify-start items-center md:items-start gap-1 md:gap-4">
                         <h3 class="text-2xl font-medium text-dark">{{ $t('n-services-item3-title') }}</h3>
                         <div class="inline-block">
