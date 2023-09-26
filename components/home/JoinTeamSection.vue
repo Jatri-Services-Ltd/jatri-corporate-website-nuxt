c09d09eec083b6725b0635ab30bfc954b79dcb05
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-09-26 12:12:26 +0600

    homepage team section overlary issue solved

--- components/home/JoinTeamSection.vue
+++ components/home/JoinTeamSection.vue
@@ -35,7 +35,7 @@
             <img class="rounded-bl-[32px]" src="~/assets/images/home/small-device-bottom-ellipse.svg" alt="Jatri Career">
           </div>
           <div class="flex items-center justify-start sm:justify-center lg:justify-start text-white h-full mx-6 lg:mx-0 pl-0 lg:pl-10 xl:pl-20 pt-5 lg:pt-0 pb-14 lg:pb-0">
-            <div class="relative md:py-5 lg:py-20">
+            <div class="relative md:py-5 lg:py-20 z-50">
               <h3 class="text-2xl md:text-[40px] xl:text-[45px] font-semibold leading-8 lg:leading-[52px] mb-4 lg:mb-6">{{ $t('n-join-team-title') }}</h3>
               <p class="text-sm lg:text-[16px] leading-5 lg:leading-6 font-medium">{{ $t('n-join-team-description-1') }}<br/> {{ $t('n-join-team-description-2') }}</p>
               <div class="mt-6 lg:mt-12">
