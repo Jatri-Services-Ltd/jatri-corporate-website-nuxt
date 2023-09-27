997d711582613c2bcb0703630445a1cd1467465d
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-09-27 11:47:06 +0600

    our services section updated

--- components/home/OurServices.vue
+++ components/home/OurServices.vue
@@ -12,8 +12,8 @@
                         <h3 class="text-2xl font-medium text-dark">{{ $t('n-services-item1-title') }}</h3>
                         <div class="inline-block">
                             <a href="https://ticket.jatri.co/bus" class="flex justify-center items-center gap-2 rounded-full bg-primary px-4 lg:px-6 py-2.5 cursor-pointer">
-                                <span data-v-35b889ec="" class="text-sm lg:text-base font-medium text-white">{{ $t('n-services-buy-ticket-button') }}</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                <span data-v-35b889ec="" class="text-sm font-medium capitalize text-white">{{ $t('n-services-buy-ticket-button') }}</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 20 20" fill="none">
                                     <path d="M10.8842 9.99987L6.75916 5.87487L7.93749 4.69653L13.2408 9.99987L7.93749 15.3032L6.75916 14.1249L10.8842 9.99987Z" fill="white"/>
                                 </svg>
                             </a>
@@ -27,8 +27,8 @@
                         <h3 class="text-2xl font-medium text-dark">{{ $t('n-services-item2-title') }}</h3>
                         <div class="inline-block">
                             <a href="https://ticket.jatri.co/launch" class="flex justify-center items-center gap-2 rounded-full bg-primary px-4 lg:px-6 py-2.5 cursor-pointer">
-                                <span data-v-35b889ec="" class="text-sm lg:text-base font-medium text-white">{{ $t('n-services-buy-ticket-button') }}</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                <span data-v-35b889ec="" class="text-sm font-medium capitalize text-white">{{ $t('n-services-buy-ticket-button') }}</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 20 20" fill="none">
                                     <path d="M10.8842 9.99987L6.75916 5.87487L7.93749 4.69653L13.2408 9.99987L7.93749 15.3032L6.75916 14.1249L10.8842 9.99987Z" fill="white"/>
                                 </svg>
                             </a>
@@ -42,8 +42,8 @@
                         <h3 class="text-2xl font-medium text-dark">{{ $t('n-services-item3-title') }}</h3>
                         <div class="inline-block">
                             <a href="https://rental.jatri.co/" class="flex justify-center items-center gap-2 rounded-full bg-primary px-4 lg:px-6 py-2.5 cursor-pointer">
-                                <span data-v-35b889ec="" class="text-sm lg:text-base font-medium text-white">{{ $t('n-services-book-now-button') }}</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                <span data-v-35b889ec="" class="text-sm font-medium capitalize text-white">{{ $t('n-services-book-now-button') }}</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 20 20" fill="none">
                                     <path d="M10.8842 9.99987L6.75916 5.87487L7.93749 4.69653L13.2408 9.99987L7.93749 15.3032L6.75916 14.1249L10.8842 9.99987Z" fill="white"/>
                                 </svg>
                             </a>
