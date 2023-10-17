554c9afc2f5d91ecdf3dcc878327908a005633e3
Saiem <shahedsaiem.jatri@gmail.com>
2023-10-17 18:16:43 +0600

    faq height fixed

--- components/jatri-card/FaqSection.vue
+++ components/jatri-card/FaqSection.vue
@@ -21,67 +21,67 @@
           <div class="flex items-center justify-center">
             <div class="flex flex-col">
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
-                <div class="flex h-12 items-center justify-between font-semibold">
-                  <p class="text-[16px] lg:text-[24px] font-medium leading-6 lg:leading-8 text-start">How can I get Dmoney Jatri card?</p>
-                  <img class="h-4 w-4 group-focus:rotate-180 h-6 w-6 transition-all ease-in-out duration-300" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
+                <div class="flex  items-center justify-between">
+                  <p class="faq-question">How can I get Dmoney Jatri card?</p>
+                  <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
                 <div class="h-0 overflow-hidden duration-300 group-focus:h-[120px] lg:group-focus:h-[60px] transition-all ease-in-out delay-75">
-                  <span class="flex items-center text-start text-[#4D4D4F] text-sm lg:text-[20px] h-full">For getting card firstly, have to have a verified Dmoney wallet. And you can get through Dmoney & Jatri. You can apply through app or activation booth/bus Counter.</span>
+                  <span class="faq-answer">For getting card firstly, have to have a verified Dmoney wallet. And you can get through Dmoney & Jatri. You can apply through app or activation booth/bus Counter.</span>
                 </div>
               </button>
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
-                <div class="flex h-12 items-center justify-between font-semibold">
-                  <p class="text-[16px] lg:text-[24px] font-medium leading-6 lg:leading-8 text-start">How can I recharge my card?</p>
-                  <img class="h-4 w-4 group-focus:rotate-180 h-6 w-6 transition-all ease-in-out duration-300" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
+                <div class="flex items-center justify-between">
+                  <p class="faq-question">How can I recharge my card?</p>
+                  <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
                 <div class="h-0 overflow-hidden duration-300 group-focus:h-[120px] lg:group-focus:h-[70px]">
-                  <span class="flex items-center text-start text-[#4D4D4F] text-sm lg:text-[20px] h-full">You can recharge from your bank account/debit or credit card. Add money from bank is totally free and for VISA and Mastercard the gateway charge is 2.1% and for Amex card the gateway charge is 3.7%</span>
+                  <span class="faq-answer">You can recharge from your bank account/debit or credit card. Add money from bank is totally free and for VISA and Mastercard the gateway charge is 2.1% and for Amex card the gateway charge is 3.7%</span>
                 </div>
               </button>
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
-                <div class="flex h-12 items-center justify-between font-semibold">
+                <div class="flex items-center justify-between">
                   <p class="text-[16px] text-start lg:text-[24px] font-medium leading-6 lg:leading-8 text-start">Is there any option to get multiple tickets through card
                     <br class="hidden xl:block"> payment?</p>
-                  <img class="h-4 w-4 group-focus:rotate-180 h-6 w-6 transition-all ease-in-out duration-300" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
+                  <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
                 <div class="h-0 overflow-hidden duration-300 group-focus:h-[60px]">
-                  <span class="flex items-center text-start text-[#4D4D4F] text-sm lg:text-[20px] h-full">No! Passengers have to collect tickets one by one.</span>
+                  <span class="faq-answer">No! Passengers have to collect tickets one by one.</span>
                 </div>
               </button>
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
-                <div class="flex h-12 items-center justify-between font-semibold">
-                  <p class="text-[16px] lg:text-[24px] font-medium leading-6 lg:leading-8 text-start">How can I check my card balance ?</p>
-                  <img class="h-4 w-4 group-focus:rotate-180 h-6 w-6 transition-all ease-in-out duration-300" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
+                <div class="flex items-center justify-between">
+                  <p class="faq-question">How can I check my card balance ?</p>
+                  <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
                 <div class="h-0 overflow-hidden duration-300 group-focus:h-[60px]">
-                  <span class="flex items-center text-start text-[#4D4D4F] text-sm lg:text-[20px] h-full">Through Dmoney app.</span>
+                  <span class="faq-answer">Through Dmoney app.</span>
                 </div>
               </button>
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
-                <div class="flex h-12 items-center justify-between font-semibold">
-                  <p class="text-[16px] lg:text-[24px] font-medium leading-6 lg:leading-8 text-start">How can I apply for a card?</p>
-                  <img class="h-4 w-4 group-focus:rotate-180 h-6 w-6 transition-all ease-in-out duration-300" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
+                <div class="flex items-center justify-between">
+                  <p class="faq-question">How can I apply for a card?</p>
+                  <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
                 <div class="h-0 overflow-hidden duration-300 group-focus:h-[60px]">
-                  <span class="flex items-center text-start text-[#4D4D4F] text-sm lg:text-[20px] h-full">You can apply through app or activation booth/bus counter.</span>
+                  <span class="faq-answer">You can apply through app or activation booth/bus counter.</span>
                 </div>
               </button>
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
-                <div class="flex h-12 items-center justify-between font-semibold">
-                  <p class="text-[16px] lg:text-[24px] font-medium leading-6 lg:leading-8 text-start">What about card expiry date?</p>
-                  <img class="h-4 w-4 group-focus:rotate-180 h-6 w-6 transition-all ease-in-out duration-300" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
+                <div class="flex items-center justify-between">
+                  <p class="faq-question">What about card expiry date?</p>
+                  <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
                 <div class="h-0 overflow-hidden duration-300 group-focus:h-[60px]">
-                  <span class="flex items-center text-start text-[#4D4D4F] text-sm lg:text-[20px] h-full">Validity date will be printed on the card.</span>
+                  <span class="faq-answer">Validity date will be printed on the card.</span>
                 </div>
               </button>
               <button class="group focus:outline-none py-6">
-                <div class="flex h-12 items-center justify-between font-semibold">
-                  <p class="text-[16px] lg:text-[24px] font-medium leading-6 lg:leading-8 text-start">Where to use the Card & accessibility of card?</p>
-                  <img class="h-4 w-4 group-focus:rotate-180 h-6 w-6 transition-all ease-in-out duration-300" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
+                <div class="flex items-center justify-between">
+                  <p class="faq-question">Where to use the Card & accessibility of card?</p>
+                  <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
                 <div class="h-0 overflow-hidden duration-300 group-focus:h-[60px]">
-                  <span class="flex items-center text-start text-[#4D4D4F] text-sm lg:text-[20px] h-full">Card is useable at all the UnionPay enabled ATM booth, POS machine as well as the Jatri ticket purchase agents across the nation.</span>
+                  <span class="faq-answer">Card is useable at all the UnionPay enabled ATM booth, POS machine as well as the Jatri ticket purchase agents across the nation.</span>
                 </div>
               </button>
             </div>
@@ -91,3 +91,15 @@
   </div>
 
 </template>
+
+<style scoped>
+    .arrow{
+      @apply h-4 w-4 group-focus:rotate-180 h-6 w-6 transition-all ease-in-out duration-300
+    }
+    .faq-question{
+      @apply text-[16px] lg:text-[24px] text-black font-medium leading-6 lg:leading-8 text-start
+    }
+    .faq-answer{
+      @apply flex items-center text-start text-[#4D4D4F] text-sm lg:text-[20px] h-full
+    }
+</style>
