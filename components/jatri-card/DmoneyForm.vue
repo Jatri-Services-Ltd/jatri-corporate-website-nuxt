f887037adf1e055cfd2048a9602883746e01b64b
Saiem <shahedsaiem.jatri@gmail.com>
2023-10-18 16:42:44 +0600

    card faq section localization added

--- components/jatri-card/DmoneyForm.vue
+++ components/jatri-card/DmoneyForm.vue
@@ -9,7 +9,7 @@ console.log("====",locale.value)
       <div class="flex flex-col lg:flex-row justify-center bg-white rounded-xl w-full" style="box-shadow: 0 3px 10px rgb(0 0 0 / 0.1);">
         <div class="background">
           <div class="pt-10 lg:pt-[60px] px-3 lg:px-12">
-              <p class="w-2/3 text-[28px] font-semibold leading-9 lg:text-[45px] lg:leading-[52px] text-white">{{ $t('card-fromLeft-header') }}</p>
+              <p class="w-4/5 lg:w-2/3 text-[28px] font-semibold leading-9 lg:text-[45px] lg:leading-[52px] text-white">{{ $t('card-fromLeft-header') }}</p>
             <p class="w-full lg:w-[60%] text-xs font-medium leading-4 mt-2 lg:mt-3 lg:text-base lg:font-normal lg:leading-6 text-white">{{ $t('card-fromLeft-content') }}</p>
           </div>
         </div>
--- components/jatri-card/FaqSection.vue
+++ components/jatri-card/FaqSection.vue
@@ -6,82 +6,79 @@
   <div class="custom-container pt-10 lg:pt-[100px] pb-10 lg:pb-[100px]">
       <div class="flex flex-col lg:flex-row justify-center">
         <div class="w-full lg:w-[40%]">
-          <p class="text-[#676769] font-medium text-base lg:text-[20px] leading-6 lg:leading-7">FAQs</p>
-          <p class="mt-2 text-black font-medium text-2xl lg:text-[32px] leading-8 lg:leading-12">Have a question? We have
-            <br class="hidden xl:block">
-            the answers.</p>
+          <p class="text-[#676769] font-medium text-base lg:text-[20px] leading-6 lg:leading-7">{{ $t('card-faq-header') }}</p>
+          <p class="mt-2 w-4/5 lg:w-2/3 text-black font-medium text-2xl lg:text-[32px] leading-8 lg:leading-12">{{ $t('card-faq-content') }}</p>
           <div class="flex gap-x-2 items-baseline">
-            <p class="mt-4 text-[#4D4D4F] text-sm leading-5 font-normal">Still need help?</p>
-            <a href="/contact-us" class="text-xs block text-primary font-normal lg:font-medium underline flex">Contact us
+            <p class="mt-4 text-[#4D4D4F] text-sm leading-5 font-normal">{{ $t('card-faq-sub-content') }}</p>
+            <a href="/contact-us" class="text-xs block text-primary font-normal lg:font-medium underline flex">{{ $t('card-faq-contact') }}
               <img src="~/assets/images/common/right-arrow.svg" alt="right sided arrow">
             </a>
           </div>
         </div>
-        <div class="w-full lg:w-[60%]">
+        <div class="w-full lg:w-[60%] mt-4 lg:mt-0">
           <div class="flex items-center justify-center">
             <div class="flex flex-col">
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
                 <div class="flex  items-center justify-between">
-                  <p class="faq-question">How can I get Dmoney Jatri card?</p>
+                  <p class="faq-question">{{ $t('card-faq-question1') }}</p>
                   <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
-                <div class="h-0 overflow-hidden duration-300 group-focus:h-[120px] lg:group-focus:h-[60px] transition-all ease-in-out delay-75">
-                  <span class="faq-answer">For getting card firstly, have to have a verified Dmoney wallet. And you can get through Dmoney & Jatri. You can apply through app or activation booth/bus Counter.</span>
+                <div class="h-0 overflow-hidden duration-300 group-focus:h-[80px] md:group-focus:h-[120px] xl:group-focus:h-[90px] transition-all ease-in-out delay-75">
+                  <span class="faq-answer">{{ $t('card-faq-answer1') }}</span>
                 </div>
               </button>
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
                 <div class="flex items-center justify-between">
-                  <p class="faq-question">How can I recharge my card?</p>
+                  <p class="faq-question">{{ $t('card-faq-question2') }}</p>
                   <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
-                <div class="h-0 overflow-hidden duration-300 group-focus:h-[120px] lg:group-focus:h-[70px]">
-                  <span class="faq-answer">You can recharge from your bank account/debit or credit card. Add money from bank is totally free and for VISA and Mastercard the gateway charge is 2.1% and for Amex card the gateway charge is 3.7%</span>
+                <div class="h-0 overflow-hidden duration-300 group-focus:h-[80px] md:group-focus:h-[120px] xl:group-focus:h-[90px]">
+                  <span class="faq-answer">{{ $t('card-faq-answer2') }}</span>
                 </div>
               </button>
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
                 <div class="flex items-center justify-between">
-                  <p class="text-base text-start lg:text-2xl font-medium leading-6 lg:leading-8 text-start">Is there any option to get multiple tickets through card
-                    <br class="hidden xl:block"> payment?</p>
+                  <p class="text-base text-start lg:text-2xl font-medium leading-6 lg:leading-8 text-start">{{ $t('card-faq-question3') }}</p>
                   <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
                 <div class="h-0 overflow-hidden duration-300 group-focus:h-[60px]">
-                  <span class="faq-answer">No! Passengers have to collect tickets one by one.</span>
+                  <span class="faq-answer">{{ $t('card-faq-answer3') }}</span>
                 </div>
               </button>
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
                 <div class="flex items-center justify-between">
-                  <p class="faq-question">How can I check my card balance ?</p>
+                  <p class="faq-question">{{ $t('card-faq-question4') }}</p>
                   <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
                 <div class="h-0 overflow-hidden duration-300 group-focus:h-[60px]">
-                  <span class="faq-answer">Through Dmoney app.</span>
+                  <span class="faq-answer">{{ $t('card-faq-answer4') }}</span>
                 </div>
               </button>
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
                 <div class="flex items-center justify-between">
-                  <p class="faq-question">How can I apply for a card?</p>
+                  <p class="faq-question">{{ $t('card-faq-question5') }}</p>
                   <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
                 <div class="h-0 overflow-hidden duration-300 group-focus:h-[60px]">
-                  <span class="faq-answer">You can apply through app or activation booth/bus counter.</span>
+                  <span class="faq-answer">{{ $t('card-faq-answer5') }}</span>
                 </div>
               </button>
               <button class="group focus:outline-none border-b border-[#EDEDED] py-3 lg:py-6">
                 <div class="flex items-center justify-between">
-                  <p class="faq-question">What about card expiry date?</p>
+                  <p class="faq-question">{{ $t('card-faq-question6') }}</p>
                   <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
                 <div class="h-0 overflow-hidden duration-300 group-focus:h-[60px]">
-                  <span class="faq-answer">Validity date will be printed on the card.</span>
+                  <span class="faq-answer">{{ $t('card-faq-answer6') }}</span>
                 </div>
               </button>
               <button class="group focus:outline-none py-6">
                 <div class="flex items-center justify-between">
-                  <p class="faq-question">Where to use the Card & accessibility of card?</p>
+                  <p class="faq-question">{{ $t('card-faq-question7') }}</p>
                   <img class="arrow" src="~/assets/images/common/down_arrow.svg" alt="down arrow">
                 </div>
-                <div class="h-0 overflow-hidden duration-300 group-focus:h-[60px]">
-                  <span class="faq-answer">Card is useable at all the UnionPay enabled ATM booth, POS machine as well as the Jatri ticket purchase agents across the nation.</span>
+                <div class="h-0 overflow-hidden duration-300 group-focus:h-[80px] md:group-focus:h-[100px] xl:group-focus:h-[90px]">
+                  <span class="faq-answer">{{ $t('card-faq-answer7') }}</span>
                 </div>
               </button>
             </div>
@@ -100,6 +97,6 @@
       @apply text-base lg:text-2xl text-black font-medium leading-6 lg:leading-8 text-start
     }
     .faq-answer{
-      @apply flex items-center text-start text-[#4D4D4F] text-sm lg:text-[20px] h-full
+      @apply flex items-center text-start text-[#4D4D4F] text-sm lg:text-[20px] h-full leading-4 lg:leading-7
     }
 </style>
