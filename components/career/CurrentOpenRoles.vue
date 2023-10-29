db8943bfea7e4dfdf2cea25dd8941279e7770eac
Saiem <shahedsaiem.jatri@gmail.com>
2023-10-29 16:44:44 +0600

    career data fetch done

--- components/career/CurrentOpenRoles.vue
+++ components/career/CurrentOpenRoles.vue
@@ -1,21 +1,32 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+const openedRoles = ref([])
+
+const config = useRuntimeConfig();
+const {data} = await useFetch(config.public.apiURL + '/api/v1/get-career-active-jobs')
+
+</script>
+
+
 <template>
     <div class="py-10 lg:py-[100px]">
         <div class="custom-container">
             <h2 class="text-[28px] lg:text-4xl leading-9 lg:leading-[44px] font-medium mb-8 lg:mb-10">Current open roles</h2>
-            <div v-if="openedRoles && openedRoles.length > 0" class="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-6">
-                <div v-for="role in openedRoles" :key="role.id" class="p-6 rounded-2xl border border-1-[#DBDBDB]">
+            <div v-if="data.data.length" class="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-6">
+                <div v-for="job in data.data" :key="data.id" class="p-6 rounded-2xl border border-1-[#DBDBDB]">
                     <div class="xl:flex items-center justify-between">
                         <div>
-                            <p class="text-base lg:text-xl font-semibold">{{ role.title }}</p>
+                            <p class="text-base lg:text-xl font-semibold">{{job.title}}</p>
                             <div class="my-2 flex gap-2 items-center">
-                                <p class="text-sm lg:text-base font-medium text-[#4D4D4F]">Salary: <span>{{ role.salary }}</span></p>
+                                <p class="text-sm lg:text-base font-medium text-[#4D4D4F]">Salary: <span>{{job.salary_range}}</span></p>
                                 <div class="h-[6px] w-[6px] bg-[#DBDBDB] rounded-full"></div>
-                                <p class="text-sm lg:text-base font-medium text-[#4D4D4F]">{{ role.employment_type }}</p>
+                                <p class="text-sm lg:text-base font-medium text-[#4D4D4F]">{{job.employment_status}}</p>
                             </div>
-                            <p class="text-sm lg:text-base font-medium text-[#4D4D4F]">Application deadline: <span v-html="role.deadline"></span></p>
+                            <p class="text-sm lg:text-base font-medium text-[#4D4D4F]">Application deadline: <span>{{job.deadline}}</span></p>
                         </div>
                         <div class="xl:pt-0 pt-4">
-                            <router-link :to="'/career/job-details/'+ role.slug" class="flex justify-center items-center gap-2 h-10 w-[130px] rounded-full border border-[#DBDBDB] bg-white pl-2">
+<!--                          :to="'/career/job-details/'+ role.slug"/-->
+                            <router-link :to="{ path: '/career/job-details/' + job.id}"  class="flex justify-center items-center gap-2 h-10 w-[130px] rounded-full border border-[#DBDBDB] bg-white pl-2">
                                 <span class="inline-block text-sm leading-5 font-medium">View details</span>
                                 <div><img class="h-4 w-4" src="/images/career/arrow-right.svg" alt=""></div>
                             </router-link>
@@ -38,17 +49,6 @@
         </div>
     </div>
 </template>
-
-<script setup>
-import { ref, onMounted } from 'vue';
-import careerData from '../../dataStore/career.js';
-const openedRoles = ref([])
-
-onMounted(() => {
-    openedRoles.value = careerData
-})
-
-</script>
 
 <style scoped>
 
