2d517054231dd6a89c3094c17dd04229d6912750
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-10-30 17:06:24 +0600

    career page added

---
+++ components/career/OurCoreValues.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="custom-container mx-auto text-center py-10 lg:py-[100px]">
+        <h1 class="text-[28px] md:text-5xl lg:text-[57px] leading-9 md:leading-[64px] text-dark mb-2 lg:mb-6 font-semibold">{{ $t('our-core-values') }}</h1>
+        <p class="text-sm md:text-base w-full lg:w-1/2 mx-auto text-secondaryDark">{{ $t('core-values-subtitle') }}</p>
+        <div class="mt-6 lg:mt-12 grid grid-cols-2 lg:grid-cols-3 gap-x-4 lg:gap-x-8 gap-y-5 md:gap-y-8">
+            <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
+                <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/stand-for-safety.svg" :alt="$t('core-value-card1-title')">
+                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card1-title') }}</h3>
+                <p class="text-sm md:text-base">{{ $t('core-value-card1-detail') }}</p>
+            </div>
+            <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
+                <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/strong-together.svg" :alt="$t('core-value-card2-title')">
+                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card2-title') }}</h3>
+                <p class="text-sm md:text-base">{{ $t('core-value-card2-detail') }}</p>
+            </div>
+            <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
+                <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/build-with-heart.svg" :alt="$t('core-value-card3-title')">
+                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card3-title') }}</h3>
+                <p class="text-sm md:text-base">{{ $t('core-value-card3-detail') }}</p>
+            </div>
+            <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
+                <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/diversity-equity-inclusion.svg" :alt="$t('core-value-card4-title')">
+                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card4-title') }}</h3>
+                <p class="text-sm md:text-base">{{ $t('core-value-card4-detail') }}</p>
+            </div>
+            <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
+                <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/be-positive.svg" :alt="$t('core-value-card5-title')">
+                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card5-title') }}</h3>
+                <p class="text-sm md:text-base">{{ $t('core-value-card5-detail') }}</p>
+            </div>
+            <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
+                <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/celebrate-progress.svg" :alt="$t('core-value-card6-title')">
+                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card6-title') }}</h3>
+                <p class="text-sm md:text-base">{{ $t('core-value-card6-detail') }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/career/WorkplaceLikeHome.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="custom-container mx-auto pb-10 lg:pb-[100px] border-b border-[#EDEDED]">
+        <div class="block lg:flex">
+            <div class="w-full lg:w-2/3 xl:w-1/2 mt-0 md:mt-[30px]">
+                <h2 class="text-[26px] md:text-4xl text-dark leading-9 md:leading-[44px] font-medium">{{ $t('location-title1') }}</h2>
+                <h2 class="text-[26px] md:text-4xl text-dark leading-9 md:leading-[44px] font-medium">{{ $t('location-title2') }}</h2>
+                <p class="text-base text-dark pb-8 border-b mr-0 md:mr-24 mt-4">{{ $t('location-subtitle') }}</p>
+                <p class="uppercase text-sm text-[#8D8D8F] mb-5 mt-8">{{ $t('our-location') }}</p>
+                <div class="grid grid-cols-2">
+                    <div>
+                        <div class="flex items-center gap-3 mb-6 md:mb-8">
+                            <img class="h-5 w-5" src="/images/career/map.svg" alt="Location">
+                            <p class="text-dark text-base md:text-xl font-medium">{{ $t('location-one') }}</p>
+                        </div>
+                        <div class="flex items-center gap-3">
+                            <img class="h-5 w-5" src="/images/career/map.svg" alt="Location">
+                            <p class="text-dark text-base md:text-xl font-medium">{{ $t('location-two') }}</p>
+                        </div>
+                    </div>
+                    <div class="">
+                        <div class="flex items-center gap-3 mb-6 md:mb-8">
+                            <img class="h-5 w-5" src="/images/career/map.svg" alt="Location">
+                            <p class="text-dark text-base md:text-xl font-medium">{{ $t('location-three') }}</p>
+                        </div>
+                        <div class="flex items-center gap-3">
+                            <img class="h-5 w-5" src="/images/career/map.svg" alt="Location">
+                            <p class="text-dark text-base md:text-xl font-medium">{{ $t('location-four') }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full lg:w-1/3 xl:w-1/2 flex justify-center lg:justify-end mt-6 lg:mt-0">
+                <img src="/images/career/workplace.png" alt="Jatri Career">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
