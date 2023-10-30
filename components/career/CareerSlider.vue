3d7bf143a8c9373a0f0d78044d0aea4281ac2832
farjanaIslamNipa <farjana.jatri@gmail.com>
2023-10-30 14:10:18 +0600

    working on career page slider

---
+++ components/career/CareerSlider.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="bg-[#F7F7F7] py-10 lg:py-[60px] relative">
+    <div class="sample-slider">
+      <swiper
+        :breakpoints="{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 20
+          },
+          1280: {
+            slidesPerView: 4,
+            spaceBetween: 32
+          }
+        }"
+        :spaceBetween="30"
+        :freeMode="{
+          enabled: true,
+          freeModeMomentum: false
+        }"
+        :loop="true"
+        :speed=2000
+        :autoplay="{
+          enabled: true,
+          delay: 0,
+          stopOnLastSlide: false,
+          waitForTransition: true
+        }"
+    
+        :modules="modules"
+        class="mySwiper"
+    >
+        <swiper-slide>
+        <div class="career-slider-img">
+          <img src="/images/career/top-slider-1.jpg" alt="">
+        </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/top-slider-2.png" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/top-slider-3.png" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/top-slider-4.png" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/top-slider-1.jpg" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/top-slider-2.png" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/top-slider-3.png" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/top-slider-4.png" alt="">
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
+    <div class="sample-slider mt-4 md:mt-8">
+      <swiper
+        :breakpoints="{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 20
+          },
+          1280: {
+            slidesPerView: 4,
+            spaceBetween: 32
+          }
+        }"
+        :spaceBetween="30"
+        :freeMode="{
+          enabled: true,
+          freeModeMomentum: false
+        }"
+        :loop="true"
+        :speed=2000
+        :autoplay="{
+          enabled: true,
+          delay: 0,
+          disableOnInteraction: true,
+          reverseDirection: true,
+          stopOnLastSlide: false,
+          waitForTransition: true
+        }"
+    
+        :modules="modules"
+        class="mySwiper"
+    >
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/bottom-slider-1.png" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/bottom-slider-2.png" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/bottom-slider-3.png" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/bottom-slider-4.png" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/top-slider-1.jpg" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/top-slider-2.png" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/top-slider-3.png" alt="">
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="career-slider-img">
+            <img src="/images/career/top-slider-4.png" alt="">
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
+    <h2 style="text-shadow: 0px 0px 26px rgba(0, 0, 0, 0.24);" class="text-white absolute text-[60px] lg:text-[120px] leading-[64px] font-[700] transform -translate-x-[50%] -translate-y-[50%] top-[50%] left-[50%] z-40">#Life@Jatri</h2>
+  </div>
+</template>
+
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { FreeMode, Autoplay } from 'swiper';
+
+const modules = [ FreeMode, Autoplay ];
+</script>
+
+<style>
+.sample-slider .swiper-wrapper{
+    transition-timing-function: linear;
+}
+.sample-slider .swiper-wrapper .swiper-slide{
+    /* height: 300px !important; */
+}
+.career-slider-img img{
+  @apply rounded-3xl
+}
+@media (max-width: 767px){
+  .career-slider-img img{
+    @apply h-[200px]
+  }
+  .sample-slider .swiper-wrapper .swiper-slide{
+    height: 200px !important;
+}
+}
+</style>
--- pages/career/index.vue
+++ pages/career/index.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
       <CareerHeader />
-        <div class="bg-[#F7F7F7] py-15 relative">
+      
+        <div class="">
+          <CareerSlider />
           <div class="flex justify-center items-center">
-              <div class="slider pl-[26px]">
+          
+              <!-- <div class="slider pl-[26px]">
                   <div class="slider-row"></div>
-              </div>
-              <h2 class="text-white absolute text-[57px] leading-[64px] font-medium">Life at Jatri</h2>
+              </div> -->
+              <!-- <h2 class="text-white absolute text-[57px] leading-[64px] font-medium">#Life@Jatri</h2> -->
           </div>
         </div>
         <CareerOurCoreValues />
@@ -26,7 +29,7 @@
 </script>
 
 <style scoped>
-.slider {
+/* .slider {
   width: 100%;
   overflow: hidden;
   height: 500px;
@@ -42,5 +45,5 @@
 @keyframes slide {
   from { background-position-x: 0; }
   to   { background-position-x: -2200px; }
-}
+} */
 </style>
