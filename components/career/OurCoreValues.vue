4b1184b01393522fe05f91cc16bd01bb3f0fda62
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-11-05 17:38:09 +0600

    career page updated

--- components/career/OurCoreValues.vue
+++ components/career/OurCoreValues.vue
@@ -5,33 +5,33 @@
         <div class="mt-6 lg:mt-12 grid grid-cols-2 lg:grid-cols-3 gap-x-4 lg:gap-x-8 gap-y-5 md:gap-y-8">
             <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
                 <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/stand-for-safety.svg" :alt="$t('core-value-card1-title')">
-                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card1-title') }}</h3>
-                <p class="text-sm md:text-base">{{ $t('core-value-card1-detail') }}</p>
+                <h3 class="text-dark text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card1-title') }}</h3>
+                <p class="text-secondaryDark text-sm md:text-base">{{ $t('core-value-card1-detail') }}</p>
             </div>
             <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
                 <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/strong-together.svg" :alt="$t('core-value-card2-title')">
-                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card2-title') }}</h3>
-                <p class="text-sm md:text-base">{{ $t('core-value-card2-detail') }}</p>
+                <h3 class="text-dark text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card2-title') }}</h3>
+                <p class="text-secondaryDark text-sm md:text-base">{{ $t('core-value-card2-detail') }}</p>
             </div>
             <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
                 <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/build-with-heart.svg" :alt="$t('core-value-card3-title')">
-                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card3-title') }}</h3>
-                <p class="text-sm md:text-base">{{ $t('core-value-card3-detail') }}</p>
+                <h3 class="text-dark text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card3-title') }}</h3>
+                <p class="text-secondaryDark text-sm md:text-base">{{ $t('core-value-card3-detail') }}</p>
             </div>
             <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
                 <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/diversity-equity-inclusion.svg" :alt="$t('core-value-card4-title')">
-                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card4-title') }}</h3>
-                <p class="text-sm md:text-base">{{ $t('core-value-card4-detail') }}</p>
+                <h3 class="text-dark text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card4-title') }}</h3>
+                <p class="text-secondaryDark text-sm md:text-base">{{ $t('core-value-card4-detail') }}</p>
             </div>
             <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
                 <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/be-positive.svg" :alt="$t('core-value-card5-title')">
-                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card5-title') }}</h3>
-                <p class="text-sm md:text-base">{{ $t('core-value-card5-detail') }}</p>
+                <h3 class="text-dark text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card5-title') }}</h3>
+                <p class="text-secondaryDark text-sm md:text-base">{{ $t('core-value-card5-detail') }}</p>
             </div>
             <div class="border border-lightGrey rounded-3xl p-4 md:p-8 bg-white text-start">
                 <img class="h-10 lg:h-20 w-10 lg:w-20 mb-4 lg:mb-12" src="/images/career/celebrate-progress.svg" :alt="$t('core-value-card6-title')">
-                <h3 class="text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card6-title') }}</h3>
-                <p class="text-sm md:text-base">{{ $t('core-value-card6-detail') }}</p>
+                <h3 class="text-dark text-xl lg:text-[28px] leading-7 lg:leading-9 font-medium md:font-semibold mb-2">{{ $t('core-value-card6-title') }}</h3>
+                <p class="text-secondaryDark text-sm md:text-base">{{ $t('core-value-card6-detail') }}</p>
             </div>
         </div>
     </div>
