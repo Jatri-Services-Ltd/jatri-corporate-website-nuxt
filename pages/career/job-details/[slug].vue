f274f2182130deb56c1f6bfdaf53d75c7a56aad9
Saiem <shahedsaiem.jatri@gmail.com>
2023-11-05 17:50:52 +0600

    validation adde din career application page

--- pages/career/job-details/[slug].vue
+++ pages/career/job-details/[slug].vue
@@ -1,7 +1,8 @@
 <script setup>
 import {reactive, ref} from 'vue';
 import axios from "axios";
-
+import careerFormSubmitValidator from "../../../validators/careerFormSubmitValidator";
+import CareerApplicationForm from "../../../components/career/CareerApplicationForm"
 
 const {$successToast, $errorToast} = useNuxtApp();
 const route = useRoute();
@@ -19,16 +20,17 @@ const applicationSubmitForm = reactive({...initialState})
 
 
 const config = useRuntimeConfig();
-const {data} = await useFetch(config.public.apiURL + '/api/v1/career-job-details/'+applicant_job_id)
+const {data} = await useFetch(config.public.apiURL + '/api/v1/career-job-details/' + applicant_job_id)
 
 const isSubmitting = ref(false)
 const uploadFile = (e) => {
   applicationSubmitForm.resume = e.target.files[0]
 }
-const removeFile= ()=>{
+const removeFile = () => {
   applicationSubmitForm.resume = ''
 }
 const handleSubmit = () => {
+  isSubmitting.value = true
   let applicationSubmitFormData = new FormData()
 
   // for (const property in applicationSubmitForm) {
@@ -36,27 +38,33 @@ const handleSubmit = () => {
   //   applicationSubmitFormData.append(`${property}`, `${applicationSubmitForm[property]}`)
   // }
 
-  applicationSubmitFormData.append("name",applicationSubmitForm.name)
-  applicationSubmitFormData.append("email",applicationSubmitForm.email)
-  applicationSubmitFormData.append("mobile_number",applicationSubmitForm.mobile_number)
-  applicationSubmitFormData.append("linkedin_profile",applicationSubmitForm.linkedin_profile)
-  applicationSubmitFormData.append("resume",applicationSubmitForm.resume)
-  applicationSubmitFormData.append("career_job_id",applicationSubmitForm.career_job_id)
+  applicationSubmitFormData.append("name", applicationSubmitForm.name)
+  applicationSubmitFormData.append("email", applicationSubmitForm.email)
+  applicationSubmitFormData.append("mobile_number", applicationSubmitForm.mobile_number)
+  applicationSubmitFormData.append("linkedin_profile", applicationSubmitForm.linkedin_profile)
+  applicationSubmitFormData.append("resume", applicationSubmitForm.resume)
+  applicationSubmitFormData.append("career_job_id", applicationSubmitForm.career_job_id)
 
-  isSubmitting.value = true
-  axios.post(config.public.apiURL+'/api/v1/career-application', applicationSubmitFormData,  {
+  axios.post(config.public.apiURL + '/api/v1/career-application', applicationSubmitFormData, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
   }).then((res) => {
     $successToast('Request submitted successfully')
-    Object.assign(applicationSubmitForm, { ...initialState });
+    Object.assign(applicationSubmitForm, {...initialState});
     openModal.value = false
   }).catch((e) => {
     $errorToast(e.response.data ? e.response.data?.message : 'Something went wrong. try again.')
   })
       .finally(() => isSubmitting.value = false)
 }
+
+provide("applicationSubmitForm", applicationSubmitForm)
+provide("uploadFile", uploadFile)
+provide("removeFile", removeFile)
+provide("handleSubmit", handleSubmit)
+provide("isSubmitting", isSubmitting)
+provide("openModal", openModal)
 </script>
 
 <template>
@@ -72,9 +80,9 @@ const handleSubmit = () => {
           <div class="relative">
             <div class="right-arrow absolute top-0 bottom-0 left-0 right-0 m-auto"></div>
           </div>
-          <p class="text-xs sm:text-sm ml-2 md:ml-3">{{data.data.title}}</p>
+          <p class="text-xs sm:text-sm ml-2 md:ml-3">{{ data.data.title }}</p>
         </div>
-        <h1 class="mt-2 text-[28px] lg:text-4xl leading-[44px] font-medium">{{data.data.title}}</h1>
+        <h1 class="mt-2 text-[28px] lg:text-4xl leading-[44px] font-medium">{{ data.data.title }}</h1>
       </div>
     </div>
     <div class="custom-container mt-10 mb-[120px]">
@@ -85,7 +93,8 @@ const handleSubmit = () => {
               <div><img class="h-6 w-6" src="/images/career/job-nature-icon.svg" alt="Job nature"></div>
               <div>
                 <p class="text-sm lg:text-base text-secondaryDark mb-1 font-inter">Job nature</p>
-                <p class="text-base lg:text-xl font-medium text-dark font-inter">{{data.data.employment_status.replaceAll('_', ' ').replaceAll('-', ' ')}}</p>
+                <p class="text-base lg:text-xl font-medium text-dark font-inter">
+                  {{ data.data.employment_status.replaceAll('_', ' ').replaceAll('-', ' ') }}</p>
               </div>
             </div>
             <div class="border-b ml-10 pt-4"></div>
@@ -93,7 +102,7 @@ const handleSubmit = () => {
               <div><img class="h-6 w-6" src="/images/career/salary-range-icon.svg" alt="Salary range"></div>
               <div>
                 <p class="text-sm lg:text-base text-secondaryDark mb-1">Salary range</p>
-                <p class="text-base lg:text-xl font-medium text-dark">{{data.data.salary_range}}</p>
+                <p class="text-base lg:text-xl font-medium text-dark">{{ data.data.salary_range }}</p>
               </div>
             </div>
             <div class="border-b ml-10 pt-4"></div>
@@ -101,15 +110,17 @@ const handleSubmit = () => {
               <div><img class="h-6 w-6" src="/images/career/job-location-icon.svg" alt="Job location"></div>
               <div>
                 <p class="text-sm lg:text-base text-secondaryDark mb-1">Job location</p>
-                <p class="text-base lg:text-xl font-medium text-dark">{{data.data.location}}</p>
+                <p class="text-base lg:text-xl font-medium text-dark">{{ data.data.location }}</p>
               </div>
-            </div>                    <div class="border-b ml-10 pt-4"></div>
+            </div>
+            <div class="border-b ml-10 pt-4"></div>
             <div class="flex items-center gap-5 pt-4">
               <div><img class="h-6 w-6" src="/images/career/deadline-icon.svg" alt="Application deadline"></div>
               <div>
                 <p class="text-sm lg:text-base text-secondaryDark mb-1">Application deadline</p>
                 <!--                          v-html="jobDetails.deadline"-->
-                <p class="text-base lg:text-xl font-medium text-dark" >{{new Date(data.data.deadline).toLocaleDateString()}}</p>
+                <p class="text-base lg:text-xl font-medium text-dark">
+                  {{ new Date(data.data.deadline).toLocaleDateString() }}</p>
               </div>
             </div>
             <div class="border-b ml-10 pt-4"></div>
@@ -117,7 +128,7 @@ const handleSubmit = () => {
               <div><img class="h-6 w-6" src="/images/career/vacancy-icon.svg" alt="Vacancy"></div>
               <div>
                 <p class="text-sm lg:text-base text-secondaryDark mb-1">Vacancy</p>
-                <p class="text-base lg:text-xl font-medium text-dark">{{data.data.no_of_vacancy}}</p>
+                <p class="text-base lg:text-xl font-medium text-dark">{{ data.data.no_of_vacancy }}</p>
               </div>
             </div>
           </div>
@@ -126,125 +137,61 @@ const handleSubmit = () => {
           <!--              v-html="jobDetails.description"-->
           <div style="font-family: Inter;" v-html="data.data.job_details"></div>
           <div class="mt-6 md:mt-12 mb-8 md:mb-12">
-            <a href="#top" @click="openModal = !openModal" class="bg-corporate text-white h-[46px] md:h-[60px] w-full md:w-[240px] rounded-full flex justify-center items-center font-medium text-base md:text-xl">Apply Now</a>
+            <a href="#top" @click="openModal = !openModal"
+               class="bg-corporate text-white h-[46px] md:h-[60px] w-full md:w-[240px] rounded-full flex justify-center items-center font-medium text-base md:text-xl">Apply
+              Now</a>
           </div>
           <div class="border rounded-[10px] px-6 py-5">
             <div class="flex gap-2 items-center w-full">
-              <div class="w-[10%]"><img class="h-11 w-full" src="/images/career/having-trouble-icon.svg" alt="Jatri career"></div>
+              <div class="w-[10%]"><img class="h-11 w-full" src="/images/career/having-trouble-icon.svg"
+                                        alt="Jatri career"></div>
               <div class="w-[90%]">
                 <p class="text-dark text-base md:text-xl font-medium mb-2">Having a trouble with applying?</p>
-                <p class="text-sm md:text-base pr-0 lg:pr-64">Send you CV or Resume at "<a class="text-[#1E88E5]" href="mailto:career@jatri.co">career@jatri.co</a>", do not forget to mention the post name in the subject of the mail.</p>
+                <p class="text-sm md:text-base pr-0 lg:pr-64">Send you CV or Resume at "<a class="text-[#1E88E5]"
+                                                                                           href="mailto:career@jatri.co">career@jatri.co</a>",
+                  do not forget to mention the post name in the subject of the mail.</p>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="modal w-[90%] md:w-[700px] rounded-2xl relative" :class="openModal ? 'modalOpen' : 'modal-hidden'">
-      <div class="px-4 md:px-6 pt-4 md:pt-6 border-b">
-        <div class="w-full flex justify-between items-center pb-4 md:pb-6">
-          <p class="text-xl md:text-[32px] font-medium">Apply now</p>
-          <button @click="openModal = !openModal"><img src="/images/career/close-icon.svg" alt="Close Modal"></button>
-        </div>
-      </div>
-      <div class="p-4 md:p-6">
-        <div class="h-[400px] md:h-auto overflow-y-scroll pb-24 md:pb-0">
-          <p class="text-sm md:text-2xl font-medium mb-4 md:mb-6 uppercase">Personal information</p>
-          <div class="block sm:flex gap-0 sm:gap-8">
-            <div class="w-full">
-              <label class="text-base font-medium text-dark pb-[10px] block" for="">Full name</label>
-              <input required v-model="applicationSubmitForm.name" type="text" class="w-full p-4 bg-[#F7F7F7] placeholder:text-sm placeholder:text-[#676769]" placeholder="Enter full name">
-            </div>
-            <div class="w-full">
-              <label class="text-base font-medium text-dark pb-[10px] block" for="">Email address</label>
-              <input required v-model="applicationSubmitForm.email" type="text" class="w-full p-4 bg-[#F7F7F7] placeholder:text-sm placeholder:text-[#676769]" placeholder="Enter email address">
-            </div>
-          </div>
-          <div class="block sm:flex gap-0 sm:gap-8 mt-6">
-            <div class="w-full">
-              <label class="text-base font-medium text-dark pb-[10px] block" for="">Mobile number</label>
-              <input required  maxlength="11" v-model="applicationSubmitForm.mobile_number" type="text" class="w-full p-4 bg-[#F7F7F7] placeholder:text-sm placeholder:text-[#676769]" placeholder="Enter mobile number">
-            </div>
-            <div class="w-full">
-              <label class="text-base font-medium text-dark pb-[10px] block" for="">LinkedIn profile</label>
-              <input required v-model="applicationSubmitForm.linkedin_profile" type="text" class="w-full p-4 bg-[#F7F7F7] placeholder:text-sm placeholder:text-[#676769]" placeholder="Enter linkedin profile link">
-            </div>
-          </div>
-          <div class="mt-8">
-            <p class="text-sm md:text-2xl text-[#676769] md:text-dark font-medium mb-4 md:mb-6">Resume / CV</p>
-            <label class="text-base font-medium text-dark pb-[10px] block" for="">Resume / CV</label>
-            <div class="relative overflow-hidden">
-              <button class="w-full border border-[#1E88E5] border-dashed py-[22px] px-5 text-start rounded-[4px]">
-                <div v-if="applicationSubmitForm.resume">
-                  <div class="flex gap-5 py-[10px]">
-                    <div class="flex gap-2 justify-between items-center w-full">
-                      <div class="flex">
-                        <img class="" src="/images/career/upload-icon.svg" alt="Upload file">
-                        <div class="flex items-center gap-x-2">
-                          <p class="text-sm font-medium">{{ applicationSubmitForm?.resume?.name }}</p>
-                          <button @click="removeFile" class="pt-1"><img src="/images/career/remove-file-icon.svg" alt="Remove cv"></button>
-                        </div>
-                      </div>
-                      <div>
-                        <input @change="uploadFile($event)" type="file" class="hidden" id="replace">
-                        <label class="text-red-700 border-b border-red-700 cursor-pointer" for="replace">replace</label>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="flex items-center gap-4" :class="applicationSubmitForm.resume ? 'hidden' : ''">
-                  <img src="/images/career/upload-icon.svg" alt="Upload file">
-                  <div>
-                    <p class="text-[#1E88E5] text-base font-medium mb-1">Upload resume / cv</p>
-                    <p class="text-xs">File format should be <b>PDF</b> and not more than <b>5MB</b></p>
-                  </div>
-                </div>
-              </button>
-              <input
-                  v-if="applicationSubmitForm.resume === ''"
-                  @change="uploadFile($event)" type="file"
-                  name="myfile"
-                  accept="application/pdf"
-                  class="absolute left-0 top-0 opacity-0 w-full h-full cursor-pointer"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="small-device-bottom-section">
-          <div class="mt-4 md:mt-12 mb-4 flex justify-center px-4">
-            <button @click="handleSubmit" class="h-10 md:h-[60px] w-full md:w-[300px] flex justify-center items-center bg-corporate text-white rounded-full text-sm md:text-xl font-medium">Submit</button>
-          </div>
-        </div>
-      </div>
+
+
+    <div class="modal w-[90%] md:w-[700px] rounded-2xl relative"  :class="openModal ? 'modalOpen' : 'modal-hidden'" v-if="openModal">
+
+<!--      Component here-->
+      <ClientOnly>
+        <CareerApplicationForm></CareerApplicationForm>
+      </ClientOnly>
     </div>
     <div @click="openModal = !openModal" class="overlay" :class="openModal ? '' : 'modal-hidden'"></div>
   </div>
 </template>
 
 <style>
-.careerDetails{
+.careerDetails {
   font-family: Inter;
 }
+
 .careerDetails ol, ul {
   list-style: inherit !important;
   margin: auto !important;
   padding: revert !important;
 }
-.job-details-header{
+
+.job-details-header {
   background-image: url('/images/career/job-details-bg.png');
   background-repeat: no-repeat;
   background-size: cover;
   background-position: top right;
 }
+
 .right-arrow {
   height: 0px;
   width: 0px;
   border: 3.8px solid;
-  border-color:
-      white
-      white
-      transparent
-      transparent;
+  border-color: white white transparent transparent;
   transform: rotate(45deg);
 }
 
@@ -277,7 +224,8 @@ const handleSubmit = () => {
   visibility: hidden;
   transition: all 0.3s ease-in-out;
 }
-.modalOpen{
+
+.modalOpen {
   z-index: 60;
   opacity: 1;
   visibility: visible;
@@ -305,8 +253,8 @@ const handleSubmit = () => {
   z-index: 5;
 }
 
-@media(max-width:767px) {
-  .small-device-bottom-section{
+@media (max-width: 767px) {
+  .small-device-bottom-section {
     position: fixed;
     bottom: 0;
     left: 0;
