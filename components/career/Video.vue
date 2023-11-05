7b2c5209e066be406e2cfe8e59fa955702406588
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-11-05 11:30:51 +0600

    video section updated

---
+++ components/career/Video.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="py-10 lg:py-[100px]">
+        <div class="custom-container">
+            <img class="w-full" src="/images/career/career-video.png" alt="">
+
+            <!-- <iframe src="https://www.youtube.com/watch?v=_Bt-n1eZDMc&ab_channel=JatriApp" width="100%" height="640"
+                frameborder="0" allowfullscreen>
+            </iframe>
+
+            <object data="http://www.youtube.com/embed/W7qWa52k-nE"
+   width="560" height="315"></object> -->
+        </div>
+    </section>
+</template>
--- pages/career/index.vue
+++ pages/career/index.vue
@@ -4,9 +4,7 @@
     <CareerSlider />
     <CareerOurCoreValues />
     <CareerBenefitAndPerks />
-    <div class="custom-container mx-auto py-10 lg:py-[100px]">
-      <img class="w-full" src="/images/career/career-video.png" alt="">
-    </div>
+    <CareerVideo />
     <CareerWorkplaceLikeHome />
     <div id="openRoles">
       <CareerCurrentOpenRoles />
