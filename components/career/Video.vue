3a98cf7652a03c5a54f23503ad629e5f90de6ba5
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-11-05 13:40:40 +0600

    career page - video section updated

--- components/career/Video.vue
+++ components/career/Video.vue
@@ -1,14 +1,27 @@
+<script setup>
+const videoPlay = ref(true);
+</script>
+
+
 <template>
     <section class="py-10 lg:py-[100px]">
         <div class="custom-container">
-            <img class="w-full" src="/images/career/career-video.png" alt="">
+            <div>
+                <div v-if="videoPlay" @click="videoPlay = !videoPlay">
+                    <img src="/images/career/career-video-poster.png" alt="Video poster" class="w-full cursor-pointer" />
+                </div>
 
-            <!-- <iframe src="https://www.youtube.com/watch?v=_Bt-n1eZDMc&ab_channel=JatriApp" width="100%" height="640"
-                frameborder="0" allowfullscreen>
-            </iframe>
-
-            <object data="http://www.youtube.com/embed/W7qWa52k-nE"
-   width="560" height="315"></object> -->
+                <div v-else class="h-[220px] md:h-[640px]">
+                    <iframe width="100%" height="100%"
+                        src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1"></iframe>
+                </div>
+            </div>
         </div>
     </section>
 </template>
+
+<style scoped>
+iframe {
+    @apply rounded-2xl md:rounded-[32px]
+}
+</style>
