201a0524303cecc801e00dff4452acf8b7e35f51
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-11-05 15:57:50 +0600

    carrer page design updated

--- components/career/BenefitAndPerks.vue
+++ components/career/BenefitAndPerks.vue
@@ -3,28 +3,28 @@
         <div class="custom-container py-10 md:py-[60px] mx-auto">
             <div class="">
                 <h1 class="text-3xl md:text-5xl lg:text-[57px] leading-9 md:leading-[64px] text-dark font-medium md:font-semibold benefit-header relative inline-block mb-1 md:mb-6">{{ $t('benefits-perks-title') }}</h1>
-                <p class="text-sm md:text-base w-full md:w-1/2 2xl:w-1/3 text-[#4D4D4F]">{{ $t('benefits-perks-subtitle') }}</p>
+                <p class="text-sm md:text-base w-full md:w-1/2 xl:w-[498px] text-secondaryDark">{{ $t('benefits-perks-subtitle') }}</p>
             </div>
             <div class="mt-8 lg:mt-16 grid sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
                 <div>
                     <img class="h-8 md:h-10 w-8 md:w-10" src="/images/career/wallet.svg" alt="Wallet">
-                    <p class="mt-4 md:mt-6 mb-2 md:mb-4 text-xl md:text-2xl font-medium">{{ $t('benefits-perks-card1-title') }}</p>
-                    <p class="text-sm md:text-base text-[#4D4D4F]">{{ $t('benefits-perks-card1-detail') }}</p>
+                    <h4 class="mt-4 md:mt-6 mb-2 md:mb-4 text-xl md:text-2xl font-medium text-dark">{{ $t('benefits-perks-card1-title') }}</h4>
+                    <p class="text-sm md:text-base text-secondaryDark">{{ $t('benefits-perks-card1-detail') }}</p>
                 </div>
                 <div>
                     <img class="h-8 md:h-10 w-8 md:w-10" src="/images/career/restaurent.svg" alt="Restaurent">
-                    <p class="mt-4 md:mt-6 mb-2 md:mb-4 text-xl md:text-2xl font-medium">{{ $t('benefits-perks-card2-title') }}</p>
-                    <p class="text-sm md:text-base text-[#4D4D4F]">{{ $t('benefits-perks-card2-detail') }}</p>
+                    <h4 class="mt-4 md:mt-6 mb-2 md:mb-4 text-xl md:text-2xl font-medium text-dark">{{ $t('benefits-perks-card2-title') }}</h4>
+                    <p class="text-sm md:text-base text-secondaryDark">{{ $t('benefits-perks-card2-detail') }}</p>
                 </div>
                 <div>
                     <img class="h-8 md:h-10 w-8 md:w-10" src="/images/career/gift.svg" alt="Gift">
-                    <p class="mt-4 md:mt-6 mb-2 md:mb-4 text-xl md:text-2xl font-medium">{{ $t('benefits-perks-card3-title') }}</p>
-                    <p class="text-sm md:text-base text-[#4D4D4F]">{{ $t('benefits-perks-card3-detail') }}</p>
+                    <h4 class="mt-4 md:mt-6 mb-2 md:mb-4 text-xl md:text-2xl font-medium text-dark">{{ $t('benefits-perks-card3-title') }}</h4>
+                    <p class="text-sm md:text-base text-secondaryDark">{{ $t('benefits-perks-card3-detail') }}</p>
                 </div>
                 <div>
                     <img class="h-8 md:h-10 w-8 md:w-10" src="/images/career/heart.svg" alt="Heart">
-                    <p class="mt-4 md:mt-6 mb-2 md:mb-4 text-xl md:text-2xl font-medium">{{ $t('benefits-perks-card4-title') }}</p>
-                    <p class="text-sm md:text-base text-[#4D4D4F]">{{ $t('benefits-perks-card4-detail') }}</p>
+                    <h4 class="mt-4 md:mt-6 mb-2 md:mb-4 text-xl md:text-2xl font-medium text-dark">{{ $t('benefits-perks-card4-title') }}</h4>
+                    <p class="text-sm md:text-base text-secondaryDark">{{ $t('benefits-perks-card4-detail') }}</p>
                 </div>
             </div>
         </div>
--- components/career/Header.vue
+++ components/career/Header.vue
@@ -3,25 +3,35 @@
         <div class="custom-container mx-auto flex justify-center items-center">
             <div class="relative text-center">
                 <div class="hidden lg:block">
-                    <div class="developer-badge absolute bg-[#48A43F] text-white px-4 py-[6px] rounded-full text-xs font-medium -left-14 -top-10">{{ $t('development') }}</div>
-                    <div class="marketing-badge absolute bg-[#1E88E5] text-white px-4 py-[6px] rounded-full text-xs font-medium -right-14 -top-10">{{ $t('marketing') }}</div>
-                    <div class="product-badge absolute bg-[#4633F0] text-white px-4 py-[6px] rounded-full text-xs font-medium -left-16 bottom-16">{{ $t('product') }}</div>
-                    <div class="business-badge absolute bg-[#F04935] text-white px-4 py-[6px] rounded-full text-xs font-medium -right-28 bottom-[70px]">{{ $t('business') }}</div>
+                    <div
+                        class="developer-badge absolute bg-success text-white px-3 py-1.5 rounded-full text-xs font-medium -left-14 -top-10">
+                        {{ $t('development') }}</div>
+                    <div
+                        class="marketing-badge absolute bg-info text-white px-4 py-1.5 rounded-full text-xs font-medium -right-14 -top-10">
+                        {{ $t('marketing') }}</div>
+                    <div
+                        class="product-badge absolute bg-[#4633F0] text-white px-5 py-1.5 rounded-full text-xs font-medium -left-16 bottom-16">
+                        {{ $t('product') }}</div>
+                    <div
+                        class="business-badge absolute bg-primary text-white px-4 py-1.5 rounded-full text-xs font-medium -right-28 bottom-[70px]">
+                        {{ $t('business') }}</div>
                 </div>
                 <div class="">
                     <h1 class="header-title">{{ $t('career-header-title-1') }}</h1>
                     <h1 class="header-title">{{ $t('career-header-title-2') }}</h1>
                     <h1 class="header-title">{{ $t('career-header-title-3') }}</h1>
-                    <p class="text-sm md:text-base text-[#4d4d4f] mb-8 md:mb-10 mx-auto mt-4 lg:mt-6">{{ $t('career-header-subtitle') }}</p>
-                    <div class="flex justify-center items-center mx-auto gap-4">
-                        <router-link to="#" class="h-[40px] md:h-[46px] w-[155px] md:w-[180px] flex justify-center items-center border border-black rounded-full text-sm md:text-base font-medium text-white bg-black">{{ $t('header-btn-one') }}</router-link>
-                        <router-link to="#" class="h-[40px] md:h-[46px] w-[155px] md:w-[193px] flex justify-center items-center gap-3 border border-black rounded-full text-sm md:text-base font-medium text-black">
-                        <span class="inline-block">{{ $t('header-btn-two') }}</span>
-                        <span class="inline-block">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
-                            <path d="M7.83355 10.4766L12.3036 6.00658L13.4819 7.18492L7.00022 13.6666L0.518555 7.18492L1.69689 6.00658L6.16689 10.4766V0.333252H7.83355V10.4766Z" fill="#151414"/>
-                            </svg>
-                        </span>
+                    <p class="text-sm md:text-base text-[#4d4d4f] mb-8 md:mb-10 mx-auto mt-4 lg:mt-6">{{
+                        $t('career-header-subtitle') }}</p>
+                    <div class="flex justify-center items-center mx-auto gap-6">
+                        <router-link to="#"
+                            class="py-[11px] px-6 border border-dark rounded-full text-sm md:text-base font-medium text-white bg-dark">{{
+                                $t('header-btn-one') }}</router-link>
+                        <router-link to="#"
+                            class="py-[11px] pl-6 pr-4 flex justify-center items-center gap-2 border border-dark rounded-full text-sm md:text-base font-medium text-dark">
+                            <span class="inline-block">{{ $t('header-btn-two') }}</span>
+                            <span class="inline-block">
+                                <img src="/images/career/down-arrow.png" alt="Down arrow">
+                            </span>
                         </router-link>
                     </div>
                 </div>
@@ -35,31 +45,42 @@
 </script>
 
 <style scoped>
-.header-title{
+.header-title {
     @apply pt-2 text-[32px] md:text-5xl lg:text-[57px] leading-10 lg:leading-[64px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#E73C2A] to-[#FFB728] text-center
 }
-.developer-badge::before{
+
+.developer-badge::before {
+    width: 20px;
+    height: 20px;
     position: absolute;
-    content: url('/images/career/developer-arrow.svg');
+    content: url('/images/career/developer-arrow.png');
     right: -14px;
     bottom: -17px;
 }
-.marketing-badge::before{
+
+.marketing-badge::before {
+    width: 20px;
+    height: 20px;
     position: absolute;
-    content: url('/images/career/marketing-arrow.svg');
+    content: url('/images/career/marketing-arrow.png');
     left: -14px;
     bottom: -17px;
 }
-.product-badge::before{
+
+.product-badge::before {
+    width: 20px;
+    height: 20px;
     position: absolute;
-    content: url('/images/career/product-arrow.svg');
-    right: -13px;
-    top: -13px;
+    content: url('/images/career/product-arrow.png');
+    right: -14px;
+    top: -17px;
 }
-.business-badge::before{
+
+.business-badge::before {
+    width: 20px;
+    height: 20px;
     position: absolute;
-    content: url('/images/career/business-arrow.svg');
-    left: -13px;
-    top: -13px;
-}
-</style>
+    content: url('/images/career/business-arrow.png');
+    left: -14px;
+    top: -17px;
+}</style>
