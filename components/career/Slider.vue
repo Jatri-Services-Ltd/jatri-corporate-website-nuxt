f9a86a3dd93aaa0081a60a8c045dba178cdd8393
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-11-07 17:26:08 +0600

    career slider updated

--- components/career/Slider.vue
+++ components/career/Slider.vue
@@ -25,7 +25,7 @@ const chunkBannerImages = {
 
     <div class="mt-4 md:mt-8">
       <MovingSlide direction="reverse">
-        <div class="slide-items">
+        <div class="slider-items2">
           <div class="slide-item" v-for="slideImage in chunkBannerImages.secondSlider" :key="slideImage">
             <img :src="'https://sgp1.digitaloceanspaces.com/storage.jatri.co/' + slideImage">
           </div>
@@ -39,28 +39,50 @@ const chunkBannerImages = {
   </div>
 </template>
 
-<style scoped> 
- .slide-items {
+<style scoped> .slide-items,
+ .slider-items2 {
    @apply flex gap-x-4 md:gap-x-8
  }
 
- .slide-item {
+ .slide-items .slide-item,
+ .slider-items2 .slide-item {
    @apply w-[604px] h-[200px] sm:h-[300px]
  }
 
- .slide-item:nth-child(2) {
+ .slide-items .slide-item:nth-child(2),
+ .slider-items2 .slide-item:last-child {
    @apply w-[286px]
  }
 
- .slide-item:nth-child(3) {
+ .slide-items .slide-item:nth-child(3),
+ .slide-items .slide-item:last-child,
+ .slider-items2 .slide-item:first-child {
    @apply w-[498px]
  }
 
- .slide-item:nth-child(4) {
+ .slide-items .slide-item:nth-child(4),
+ .slider-items2 .slide-item:nth-child(3) {
    @apply w-[392px]
  }
 
  .slide-item img {
    @apply w-full h-full rounded-3xl object-cover
  }
+
+ /* Left to right slider */
+ /* .slider-items2 .slide-item {
+   @apply w-[604px] h-[200px] sm:h-[300px]
+ } */
+
+ /* .slider-items2 .slide-item:first-child {
+   @apply w-[498px]
+ } */
+
+ /* .slider-items2 .slide-item:nth-child(3) {
+   @apply w-[392px]
+ } */
+/* 
+ .slider-items2 .slide-item:last-child {
+   @apply w-[286px]
+ } */
 </style>
